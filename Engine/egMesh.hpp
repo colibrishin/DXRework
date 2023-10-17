3f8708d9112061689b4171279a03dcbbb6b3ba05
Colibri Shin <colibrishin@gmail.com>
2023-10-17 20:41:15 +0900

    mesh: change inheritance

--- Engine/egMesh.hpp
+++ Engine/egMesh.hpp
@@ -10,30 +10,29 @@
 #include "egD3Device.hpp"
 #include "egRenderPipeline.hpp"
 #include "egRenderable.hpp"
+#include "egResource.hpp"
 
 
-namespace Engine::Abstract
+namespace Engine::Resources
 {
 	using namespace DirectX::SimpleMath;
 	using Microsoft::WRL::ComPtr;
 
-	class Mesh : public Renderable
+	class Mesh : public Abstract::Resource
 	{
 	public:
 		virtual ~Mesh() override = default;
 		void Initialize() override;
+		void Render() override;
+		
+		void Unload() override;
 
-		virtual void Load() = 0;
 		virtual void SetPath(const std::filesystem::path& path) { m_path_ = path; }
 
 		UINT GetIndexCount() const { return m_indices_.size(); }
 
 	protected:
-		Mesh() = default;
-
-	public:
-		void Update() override;
-		void Render() override;
+		Mesh(std::filesystem::path path) : Resource(std::move(path)) { }
 
 	protected:
 		std::vector<VertexElement> m_vertices_;
@@ -56,14 +55,16 @@ namespace Engine::Abstract
 		Graphic::D3Device::CreateBuffer<UINT>(D3D11_BIND_INDEX_BUFFER, m_indices_.size(), m_index_buffer_.ReleaseAndGetAddressOf(), m_indices_.data());
 	}
 
-	inline void Mesh::Update()
-	{
-	}
-
 	inline void Mesh::Render()
 	{
 		Graphic::RenderPipeline::BindVertexBuffer(m_vertex_buffer_.Get());
 		Graphic::RenderPipeline::BindIndexBuffer(m_index_buffer_.Get());
 		Graphic::RenderPipeline::SetTopology(m_topology);
+	}
+
+	inline void Mesh::Unload()
+	{
+		m_vertex_buffer_->Release();
+		m_index_buffer_->Release();
 	}
 }
