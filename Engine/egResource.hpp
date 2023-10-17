b346b49fa30202d076485dee20d0861e21796448
Colibri Shin <colibrishin@gmail.com>
2023-10-17 20:40:45 +0900

    feat: add resource

---
+++ Engine/egResource.hpp
@@ -0,0 +1,27 @@
+#pragma once
+#include <filesystem>
+
+#include "egRenderable.hpp"
+
+namespace Engine::Abstract
+{
+	class Resource : public Renderable
+	{
+	public:
+		virtual ~Resource() override = default;
+
+		virtual void Load() = 0;
+		virtual void Unload() = 0;
+
+		const std::filesystem::path& GetPath() const
+		{
+			return m_path_;
+		}
+
+	protected:
+		Resource(std::filesystem::path path) : m_path_(std::move(path)) {}
+
+	private:
+		std::filesystem::path m_path_;
+	};
+}
