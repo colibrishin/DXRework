4f68c5da3d793773d4420bf786ab88dc99ba0ae0
Colibri Shin <colibrishin@gmail.com>
2023-10-17 22:00:55 +0900

    project: update

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -5,6 +5,7 @@
 #include "Client.h"
 #include "clTestScene.hpp"
 #include "framework.h"
+#include "../Engine/egResourceManager.hpp"
 #include "../Engine/egSceneManager.hpp"
 
 // TODO: This is an example of a library function
@@ -12,8 +13,11 @@ namespace Client
 {
 	void fnClient()
 	{
-		Engine::Manager::SceneManager::GetInstance()->AddScene<Scene::TestScene>();
-		Engine::Manager::SceneManager::GetInstance()->SetActive<Scene::TestScene>();
+		Engine::GetResourceManager()->AddResource<Engine::Resources::Texture>(L"TestTexture", std::make_shared<Engine::Resources::Texture>(L"./Texture.png"));
+		Engine::GetResourceManager()->AddResource<Engine::Resources::Mesh>(L"TriangleMesh", std::make_shared<Client::Mesh::TriangleMesh>());
+
+		Engine::GetSceneManager()->AddScene<Scene::TestScene>();
+		Engine::GetSceneManager()->SetActive<Scene::TestScene>();
 	}
 }
 
