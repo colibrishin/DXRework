4d50ac5ad2b3d357ae93db40031b6c0a1e5cbb47
Colibri Shin <colibrishin@gmail.com>
2023-10-19 08:51:17 +0900

    feat: add spheremesh

---
+++ Client/clSphereMesh.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include "../Engine/egMesh.hpp"
+#include "GeometricPrimitive.h"
+
+namespace Client::Mesh
+{
+	class SphereMesh final : public Engine::Resources::Mesh
+	{
+	public:
+		SphereMesh();
+		~SphereMesh() override = default;
+
+		void PreUpdate() override;
+		void Update() override;
+		void PreRender() override;
+		void Load() override;
+		void Initialize() override;
+
+	private:
+
+	};
+
+	inline SphereMesh::SphereMesh() : Mesh("")
+	{
+		SphereMesh::Initialize();
+	}
+
+	inline void SphereMesh::PreUpdate()
+	{
+	}
+
+	inline void SphereMesh::Update()
+	{
+	}
+
+	inline void SphereMesh::PreRender()
+	{
+	}
+
+	inline void SphereMesh::Load()
+	{
+		GeometricPrimitive::VertexCollection vertices;
+		GeometricPrimitive::IndexCollection indices;
+		GeometricPrimitive::CreateSphere(vertices, indices, 1.f, 16, false);
+
+		for (const auto vertex : vertices)
+		{
+			m_vertices_.push_back(Engine::VertexElement{ vertex.position, vertex.normal, {1.0f, 0.0f, 0.0f, 1.0f}, vertex.textureCoordinate });
+		}
+
+		for (const auto index : indices)
+		{
+			m_indices_.push_back(static_cast<UINT>(index));
+		}
+	}
+
+	inline void SphereMesh::Initialize()
+	{
+		Mesh::Initialize();
+	}
+}
