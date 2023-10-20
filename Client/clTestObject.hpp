8470bf3a10732018876fb665ead1619e36fae959
Colibri Shin <colibrishin@gmail.com>
2023-10-20 16:17:31 +0900

    test: add collider

--- Client/clTestObject.hpp
+++ Client/clTestObject.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "clTriangleMesh.hpp"
+#include "../Engine/egCollider.hpp"
 #include "../Engine/egManagerHelper.hpp"
 #include "../Engine/egTexture.hpp"
 #include "../Engine/egObject.hpp"
@@ -38,6 +39,10 @@ namespace Client::Object
 		const auto tr = GetComponent<Engine::Component::Transform>().lock();
 		tr->SetPosition(Vector3(0.0f, 0.0f, 0.0f));
 		tr->SetScale(Vector3::One);
+
+		AddComponent<Engine::Component::Collider>();
+		const auto cldr = GetComponent<Engine::Component::Collider>().lock();
+		cldr->SetType(Engine::BOUNDING_TYPE_SPHERE);
 
 		AddResource(Engine::GetResourceManager()->GetResource<Engine::Resources::Mesh>(L"SphereMesh"));
 		AddResource(Engine::GetResourceManager()->GetResource<Engine::Resources::Texture>(L"TestTexture"));
