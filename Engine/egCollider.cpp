45f688d6c8f206b6a69fa7b404b14851c58990d0
Colibri Shin <colibrishin@gmail.com>
2023-10-20 16:13:45 +0900

    feat: add collider

---
+++ Engine/egCollider.cpp
@@ -0,0 +1,106 @@
+#include "pch.hpp"
+#include "egCollider.hpp"
+
+namespace Engine::Component
+{
+	void Collider::SetPosition(const Vector3& position)
+	{
+		m_position_ = position;
+	}
+
+	void Collider::SetRotation(const Quaternion& rotation)
+	{
+		m_rotation_	= rotation;
+	}
+
+	void Collider::SetSize(const Vector3& size)
+	{
+		m_size_ = size;
+	}
+
+	void Collider::SetType(const eBoundingType type)
+	{
+		m_type_ = type;
+		UpdateBoundings();
+	}
+
+	bool Collider::Intersects(Collider& other) const
+	{
+		if (m_type_ == BOUNDING_TYPE_BOX)
+		{
+			return other.Intersects_GENERAL_TYPE(m_boundings_.box);
+		}
+		else if (m_type_ == BOUNDING_TYPE_SPHERE)
+		{
+			return other.Intersects_GENERAL_TYPE(m_boundings_.sphere);
+		}
+		else if (m_type_ == BOUNDING_TYPE_FRUSTUM)
+		{
+			assert(false);
+		}
+
+		return false;
+	}
+
+	bool Collider::Contains(Collider& other) const
+	{
+		if (m_type_ == BOUNDING_TYPE_BOX)
+		{
+			return other.Contains_GENERAL_TYPE(m_boundings_.box);
+		}
+		else if (m_type_ == BOUNDING_TYPE_SPHERE)
+		{
+			return other.Contains_GENERAL_TYPE(m_boundings_.sphere);
+		}
+		else if (m_type_ == BOUNDING_TYPE_FRUSTUM)
+		{
+			assert(false);
+		}
+
+		return false;
+	}
+
+	void Collider::GenerateFromMesh(const std::weak_ptr<Resources::Mesh>& mesh)
+	{
+		const auto mesh_obj = mesh.lock();
+
+		std::vector<Vector3> vertices;
+		std::transform(std::execution::par, mesh_obj->m_vertices_.begin(), mesh_obj->m_vertices_.end(), vertices.begin(), [](const auto& vertex)
+		{
+			return vertex.position;
+		});
+
+		if (m_type_ == BOUNDING_TYPE_BOX)
+		{
+			BoundingOrientedBox::CreateFromPoints(m_boundings_.box, mesh_obj->m_vertices_.size(), vertices.data(), sizeof(Vector3));
+		}
+		else if (m_type_ == BOUNDING_TYPE_SPHERE)
+		{
+			BoundingSphere::CreateFromPoints(m_boundings_.sphere, mesh_obj->m_vertices_.size(), vertices.data(), sizeof(Vector3));
+		}
+		else if (m_type_ == BOUNDING_TYPE_FRUSTUM)
+		{
+			assert(false);
+		}
+	}
+
+	void Collider::UpdateBoundings()
+	{
+		if (m_type_ == BOUNDING_TYPE_BOX)
+		{
+			SetPosition_GENERAL_TYPE(m_boundings_.box, m_position_);
+			SetSize_GENERAL_TYPE(m_boundings_.box, m_size_);
+			SetRotation_GENERAL_TYPE(m_boundings_.box, m_rotation_);
+		}
+		else if (m_type_ == BOUNDING_TYPE_SPHERE)
+		{
+			SetPosition_GENERAL_TYPE(m_boundings_.box, m_position_);
+			SetSize_GENERAL_TYPE(m_boundings_.sphere, m_size_);
+			m_rotation_ = Quaternion::Identity;
+		}
+		else if (m_type_ == BOUNDING_TYPE_FRUSTUM)
+		{
+			assert(false);
+		}
+	}
+}
---
+++ Engine/egCollider.hpp
@@ -0,0 +1,223 @@
+#pragma once
+#include <DirectXCollision.h>
+#include <execution>
+
+#include "egComponent.hpp"
+#include "egMesh.hpp"
+#include "egTransform.hpp"
+#include "egObject.hpp"
+
+namespace Engine::Component
+{
+	using namespace DirectX;
+
+	class Collider : public Abstract::Component
+	{
+	public:
+		Collider(const std::weak_ptr<Abstract::Object>& owner);
+		~Collider() override = default;
+
+		void SetDirtyWithTransform(const bool dirty) { m_bDirtyByTransform = dirty; }
+
+		void SetPosition(const Vector3& position);
+		void SetRotation(const Quaternion& rotation);
+		void SetSize(const Vector3& size);
+		void SetType(const eBoundingType type);
+		void SetCollided(const bool collided) { m_bCollided = collided; }
+
+		bool Intersects(Collider& other) const;
+		bool Contains(Collider& other) const;
+
+		bool GetCollided() const { return m_bCollided; }
+		bool HasCollisionStarted() const { return !m_bPreviousCollided && m_bCollided; }
+		bool HasCollisionEnd() const { return m_bPreviousCollided && !m_bCollided; }
+
+		void GenerateFromMesh(const std::weak_ptr<Resources::Mesh>& mesh);
+
+		void Initialize() override;
+		void PreUpdate() override;
+		void Update() override;
+		void PreRender() override;
+		void Render() override;
+
+	private:
+		template <typename T>
+		bool Intersects_GENERAL_TYPE(const T& other)
+		{
+			if (m_type_ == BOUNDING_TYPE_BOX)
+			{
+				return As<BoundingOrientedBox>().Intersects(other);
+			}
+			else if (m_type_ == BOUNDING_TYPE_SPHERE)
+			{
+				return As<BoundingSphere>().Intersects(other);
+			}
+			else if (m_type_ == BOUNDING_TYPE_FRUSTUM)
+			{
+				assert(false);
+			}
+
+			return false;
+		}
+
+		template <typename T>
+		bool Contains_GENERAL_TYPE(const T& other)
+		{
+			if (m_type_ == BOUNDING_TYPE_BOX)
+			{
+				return As<BoundingOrientedBox>().Contains(other);
+			}
+			else if (m_type_ == BOUNDING_TYPE_SPHERE)
+			{
+				return As<BoundingSphere>().Contains(other);
+			}
+			else if (m_type_ == BOUNDING_TYPE_FRUSTUM)
+			{
+				assert(false);
+			}
+
+			return false;
+		}
+
+		template <typename T>
+		static void SetSize_GENERAL_TYPE(T& value, const Vector3& size)
+		{
+			if constexpr (std::is_same_v<T, BoundingOrientedBox>)
+			{
+				value.Extents = size / 2;
+			}
+			else if constexpr (std::is_same_v<T, BoundingSphere>)
+			{
+				value.Radius = size.Length();
+			}
+			else if constexpr (std::is_same_v<T, BoundingFrustum>)
+			{
+				assert(false);
+			}
+			else
+			{
+				throw std::exception("Invalid type");
+			}
+		}
+
+		template <typename T>
+		void SetRotation_GENERAL_TYPE(T& value, const Quaternion& rotation)
+		{
+			if constexpr (std::is_same_v<T, BoundingOrientedBox>)
+			{
+				value.Orientation = rotation;
+			}
+			else if constexpr (std::is_same_v<T, BoundingSphere>)
+			{
+				return;
+			}
+			else if constexpr (std::is_same_v<T, BoundingFrustum>)
+			{
+				assert(false);
+			}
+			else
+			{
+				throw std::exception("Invalid type");
+			}
+		}
+
+		template <typename T>
+		void SetPosition_GENERAL_TYPE(T& value, const Vector3& position)
+		{
+			if constexpr (std::is_same_v<T, BoundingFrustum>)
+			{
+				value.Origin = position;
+				return;
+			}
+
+			As<T>().Center = position;
+		}
+
+		template <typename T>
+		T As() 
+		{
+			if constexpr (std::is_same_v<T, BoundingOrientedBox>)
+			{
+				return m_boundings_.box;
+			}
+			else if constexpr (std::is_same_v<T, BoundingSphere>)
+			{
+				return m_boundings_.sphere;
+			}
+			else if constexpr (std::is_same_v<T, BoundingFrustum>)
+			{
+				assert(false);
+			}
+
+			throw std::exception("Invalid type");
+		}
+
+		void UpdateBoundings();
+
+		bool m_bDirtyByTransform;
+		bool m_bPreviousCollided;
+		bool m_bCollided;
+
+		Vector3 m_position_;
+		Vector3 m_size_;
+		Quaternion m_rotation_;
+
+		eBoundingType m_type_;
+		BoundingGroup m_boundings_;
+
+	};
+
+	inline Collider::Collider(const std::weak_ptr<Abstract::Object>& owner) : Component(owner),
+	                                                                          m_bDirtyByTransform(false),
+	                                                                          m_bPreviousCollided(false),
+	                                                                          m_bCollided(false),
+	                                                                          m_position_(Vector3::Zero),
+	                                                                          m_size_(Vector3::One),
+	                                                                          m_rotation_(Quaternion::Identity),
+	                                                                          m_type_(BOUNDING_TYPE_BOX),
+	                                                                          m_boundings_({})
+	{
+	}
+
+	inline void Collider::Initialize()
+	{
+		if(const auto mesh = GetOwner().lock()->GetResource<Resources::Mesh>().lock())
+		{
+			GenerateFromMesh(mesh);
+		}
+
+		if (const auto tr = GetOwner().lock()->GetComponent<Transform>().lock(); m_bDirtyByTransform)
+		{
+			m_position_ = tr->GetPosition();
+			m_size_ = tr->GetScale();
+			m_rotation_ = tr->GetRotation();
+
+			UpdateBoundings();
+		}
+	}
+
+	inline void Collider::PreUpdate()
+	{
+		m_bPreviousCollided = m_bCollided;
+	}
+
+	inline void Collider::Update()
+	{
+		if (const auto tr = GetOwner().lock()->GetComponent<Transform>().lock(); m_bDirtyByTransform)
+		{
+			m_position_ = tr->GetPosition();
+			m_size_ = tr->GetScale();
+			m_rotation_ = tr->GetRotation();
+
+			UpdateBoundings();
+		}
+	}
+
+	inline void Collider::PreRender()
+	{
+	}
+
+	inline void Collider::Render()
+	{
+	}
+}
