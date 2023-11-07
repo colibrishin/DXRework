895f9270e6df457680244c56ff54de582c459d5b
Colibri Shin <colibrishin@gmail.com>
2023-11-08 02:03:06 +0900

    misc: add vector max element getter

--- Engine/egHelper.hpp
+++ Engine/egHelper.hpp
@@ -2,6 +2,8 @@
 #include "egApplication.hpp"
 #include "egCommon.hpp"
 
+#undef max
+
 namespace Engine
 {
 	inline float GetDeltaTime()
@@ -20,5 +22,10 @@ namespace Engine
 	static bool IsSamePolarity(const float v1, const float v2)
 	{
 		return std::copysign(1.0f, v1) == std::copysign(1.0f, v2);
+	}
+
+	static float MaxElement(const Vector3& v)
+	{
+		return std::max(std::max(v.x, v.y), v.z);
 	}
 }
