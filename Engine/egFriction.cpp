2e930ed04c729f71cea9ec113110a416e6d131b4
Colibri Shin <colibrishin@gmail.com>
2023-11-17 22:06:19 +0900

    physics: move friction velocity guard

--- Engine/egFriction.cpp
+++ Engine/egFriction.cpp
@@ -1,5 +1,6 @@
 #include "pch.hpp"
 #include "egPhysics.h"
+#include "egFriction.h"
 
 #undef max
 
@@ -23,5 +24,23 @@ namespace Engine::Physics
 	inline Vector3 EvalDrag(const Vector3& vel, float k)
 	{
 		return -vel * k;
+	}
+
+	void FrictionVelocityGuard(Vector3& evaluated_velocity, const Vector3& friction)
+	{
+		const Vector3 Undo = evaluated_velocity - friction;
+
+		if (!IsSamePolarity(evaluated_velocity.x, Undo.x))
+		{
+			evaluated_velocity.x = 0.0f;
+		}
+		if (!IsSamePolarity(evaluated_velocity.y, Undo.y))
+		{
+			evaluated_velocity.y = 0.0f;
+		}
+		if (!IsSamePolarity(evaluated_velocity.z, Undo.z))
+		{
+			evaluated_velocity.z = 0.0f;
+		}
 	}
 }
