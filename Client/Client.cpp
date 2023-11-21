fbdf414109042a84547d1abac11c9f00df63a651
Colibri Shin <colibrishin@gmail.com>
2023-11-22 03:09:23 +0900

    client: use moved mesh from engine

--- Client/Client.cpp
+++ Client/Client.cpp
@@ -5,14 +5,14 @@
 #include "Client.h"
 
 #include "clBackSphereMesh.hpp"
-#include "clCubeMesh.hpp"
 #include "clGiftBox.hpp"
-#include "clSphereMesh.hpp"
 #include "clTestScene.hpp"
 #include "framework.h"
 #include "../Engine/egFont.hpp"
 #include "../Engine/egResourceManager.hpp"
 #include "../Engine/egSceneManager.hpp"
+#include "../Engine/egCubeMesh.hpp"
+#include "../Engine/egSphereMesh.hpp"
 
 // TODO: This is an example of a library function
 namespace Client
@@ -23,9 +23,10 @@ namespace Client
 		Engine::GetResourceManager().AddResource<Engine::Resources::Texture>(L"Sky", std::make_shared<Engine::Resources::Texture>(L"./Sky.jpg"));
 		Engine::GetResourceManager().AddResource<Engine::Resources::NormalMap>(L"TestNormalMap", std::make_shared<Engine::Resources::NormalMap>(L"./Texture-Normal.png"));
 		Engine::GetResourceManager().AddResource<Engine::Resources::Mesh>(L"TriangleMesh", std::make_shared<Client::Mesh::TriangleMesh>());
-		Engine::GetResourceManager().AddResource<Engine::Resources::Mesh>(L"CubeMesh", std::make_shared<Client::Mesh::CubeMesh>());
 		Engine::GetResourceManager().AddResource<Engine::Resources::Mesh>(L"Giftbox", std::make_shared<Client::Mesh::GiftBox>());
-		Engine::GetResourceManager().AddResource<Engine::Resources::Mesh>(L"SphereMesh", std::make_shared<Client::Mesh::SphereMesh>());
+		Engine::GetResourceManager().AddResource<Engine::Resources::Mesh>(L"CubeMesh", std::make_shared<Engine::Mesh::CubeMesh>());
+		Engine::GetResourceManager().AddResource<Engine::Resources::Mesh>(L"SphereMesh", std::make_shared<Engine::Mesh::SphereMesh>());
+
 		Engine::GetResourceManager().AddResource<Engine::Resources::Mesh>(L"BackSphereMesh", std::make_shared<Client::Mesh::BackSphereMesh>());
 		Engine::GetResourceManager().AddResource<Engine::Resources::Font>(L"DefaultFont", std::make_shared<Engine::Resources::Font>("./consolas.spritefont"));
 
