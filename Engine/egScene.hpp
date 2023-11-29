17e7c66e70ecff903091da59b2961788960ab8ec
Colibri Shin <colibrishin@gmail.com>
2023-11-30 01:40:57 +0900

    scene: move decleration, add componet cache

--- Engine/egScene.hpp
+++ Engine/egScene.hpp
@@ -30,57 +30,8 @@ namespace Engine
 		void Render(const float dt) override;
 		void FixedUpdate(const float& dt) override;
 
-		EntityID AddGameObject(const StrongObject& obj, eLayerType layer)
-		{
-			m_layers[layer]->AddGameObject(obj);
-			m_cached_objects_.emplace(obj->GetID(), obj);
-
-			if (const auto tr = obj->GetComponent<Component::Transform>().lock())
-			{
-				UpdatePosition(obj);
-			}
-
-			return obj->GetID();
-		}
-
-		void RemoveGameObject(const EntityID id, eLayerType layer)
-		{
-			const auto obj = m_cached_objects_[id];
-
-			if (const auto locked = obj.lock())
-			{
-				const auto tr = locked->GetComponent<Component::Transform>().lock();
-				bool updated = false;
-
-				if (tr)
-				{
-					const auto prev_pos = tr->GetPreviousPosition() + Vector3::One * g_octree_negative_round_up;
-					const auto pos = tr->GetPosition() + Vector3::One * g_octree_negative_round_up;
-
-					auto& prev_pos_set = m_object_position_tree_(static_cast<int>(prev_pos.x), static_cast<int>(prev_pos.y), static_cast<int>(prev_pos.z));
-					auto& pos_set = m_object_position_tree_(static_cast<int>(pos.x), static_cast<int>(pos.y), static_cast<int>(pos.z));
-
-					if (prev_pos_set.contains(obj))
-					{
-						prev_pos_set.erase(obj);
-						updated = true;
-					}
-					if (pos_set.contains(obj))
-					{
-						pos_set.erase(obj);
-						updated = true;
-					}
-				}
-
-				if (!updated)
-				{
-					// @todo: add task for refreshing octree.
-				}
-			}
-
-			m_cached_objects_.erase(id);
-			m_layers[layer]->RemoveGameObject(id);
-		}
+		EntityID AddGameObject(const StrongObject& obj, eLayerType layer);
+		void RemoveGameObject(const EntityID id, eLayerType layer);
 
 		std::vector<WeakObject> GetGameObjects(eLayerType layer)
 		{
@@ -102,15 +53,6 @@ namespace Engine
 			return m_mainCamera_;
 		}
 
-		void ChangeLayer(EntityID id, eLayerType type)
-		{
-			if (const auto obj = FindGameObject(id).lock())
-			{
-				m_layers[obj->GetLayer()]->RemoveGameObject(id);
-				m_layers[type]->AddGameObject(obj);
-			}
-		}
-
 		auto serialized_layer_begin() noexcept
 		{
 			return m_layers.begin();
@@ -119,6 +61,30 @@ namespace Engine
 		auto serialized_layer_end() noexcept
 		{
 			return m_layers.end();
+		}
+
+		template <typename T>
+		void AddComponent(const WeakComponent& component)
+		{
+			if (m_cached_objects_.contains(component.lock()->GetOwner().lock()->GetID()))
+			{
+				m_cached_components_[typeid(T)].insert(component);
+			}
+		}
+
+		template <typename T>
+		void RemoveComponent(const WeakComponent& component)
+		{
+			if (m_cached_objects_.contains(component.lock()->GetOwner().lock()->GetID()))
+			{
+				m_cached_components_[typeid(T)].erase(component);
+			}
+		}
+
+		template <typename T>
+		const std::set<WeakComponent, ComponentPriorityComparer>& GetComponents()
+		{
+			return m_cached_components_[typeid(T)];
 		}
 
 		void UpdatePosition(const WeakObject& obj);
@@ -130,6 +96,7 @@ namespace Engine
 		WeakCamera m_mainCamera_;
 		std::map<eLayerType, StrongLayer> m_layers;
 		std::map<EntityID, WeakObject> m_cached_objects_;
+		std::map<const std::type_index, std::set<WeakComponent, ComponentPriorityComparer>> m_cached_components_;
 		Octree<std::set<WeakObject, WeakObjComparer>> m_object_position_tree_;
 
 	};
