01e172f886a54ca75a04ba72e9702337158fdb1d
Colibri Shin <colibrishin@gmail.com>
2023-12-01 12:52:07 +0900

    physics: impulse does not modify position
    impulse will return the penetration vector instead.

--- Engine/egElastic.h
+++ Engine/egElastic.h
@@ -2,5 +2,5 @@
 
 namespace Engine::Physics
 {
-	void EvalImpulse(Vector3& pos1, Vector3& pos2, const Vector3& point, float penetration, const Vector3& normal, float invm1, float invm2, const Vector3& rv1, const Vector3& rv2, const Vector3& vel1, const Vector3& vel2, const DirectX::XMFLOAT3X3& inertiaT1, const DirectX::XMFLOAT3X3& inertiaT2, Vector3& linear1, Vector3& linear2, Vector3& angular1, Vector3& angular2);
+	void EvalImpulse(const Vector3& pos1, const Vector3& pos2, const Vector3& point, float penetration, const Vector3& normal, float invm1, float invm2, const Vector3& rv1, const Vector3& rv2, const Vector3& vel1, const Vector3& vel2, const DirectX::XMFLOAT3X3& inertiaT1, const DirectX::XMFLOAT3X3& inertiaT2, Vector3& linear1, Vector3& linear2, Vector3& angular1, Vector3& angular2, Vector3& penetrationA, Vector3& penetrationB);
 }
