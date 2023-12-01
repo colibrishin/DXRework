6fa9dcadd18c16241323e6fc5bc3e9dd33112bae
Colibri Shin <colibrishin@gmail.com>
2023-12-02 00:56:11 +0900

    d3device: add d2d

--- Engine/pch.hpp
+++ Engine/pch.hpp
@@ -45,11 +45,13 @@
 #include "StepTimer.hpp"
 
 #pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d2d1.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 #pragma comment(lib, "dxguid.lib")
 
 #include <d3d11.h>
+#include <d2d1.h>
 #include <d3dcompiler.h>
 #include <dxcapi.h>
 #include <directxmath.h>
