1c14e938e40bedc275d34e79284e9a791b5e0f57
Colibri Shin <colibrishin@gmail.com>
2023-12-01 12:52:07 +0900

    physics: impulse does not modify position
    impulse will return the penetration vector instead.

--- Engine/egElastic.cpp
+++ Engine/egElastic.cpp
@@ -9,15 +9,15 @@
 
 namespace Engine::Physics
 {
-	void EvalImpulse(Vector3& pos1, Vector3& pos2, const Vector3& point, float penetration, const Vector3& normal, float invm1, float invm2, const Vector3& rv1, const Vector3& rv2, const Vector3& vel1, const Vector3& vel2, const XMFLOAT3X3& inertiaT1, const XMFLOAT3X3& inertiaT2, Vector3& linear1, Vector3& linear2, Vector3& angular1, Vector3& angular2)
+	void EvalImpulse(const Vector3& pos1, const Vector3& pos2, const Vector3& point, float penetration, const Vector3& normal, float invm1, float invm2, const Vector3& rv1, const Vector3& rv2, const Vector3& vel1, const Vector3& vel2, const XMFLOAT3X3& inertiaT1, const XMFLOAT3X3& inertiaT2, Vector3& linear1, Vector3& linear2, Vector3& angular1, Vector3& angular2, Vector3& penetrationA, Vector3& penetrationB)
 	{
 		const float total_mass = invm1 + invm2;
 
 		const float penetration1 = (penetration * (invm1 / total_mass));
 		const float penetration2 = (penetration * (invm2 / total_mass));
 
-		pos1 -= normal * penetration1;
-		pos2 += normal * penetration2;
+		penetrationA = normal * penetration1;
+		penetrationB = normal * penetration2;
 
 		const Vector3 rel1 = point - pos1;
 		const Vector3 rel2 = point - pos2;
