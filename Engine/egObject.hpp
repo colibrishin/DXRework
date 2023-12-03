e088608a3e865bcfbb9979bfcb176ca5c989598a
Colibri Shin <colibrishin@gmail.com>
2023-12-03 18:44:00 +0900

    object: add local id, prep for serialization

--- Engine/egObject.hpp
+++ Engine/egObject.hpp
@@ -1,18 +1,12 @@
 #pragma once
 #include <map>
-#include <ranges>
-
 #include <set>
-#include <typeindex>
 
 #include "egActor.hpp"
 #include "egComponent.hpp"
-
-#include "egRenderable.hpp"
+#include "egScene.hpp"
 #include "egResource.hpp"
 #include <boost/make_shared.hpp>
-
-#include "egScene.hpp"
 
 namespace Engine::Component
 {
@@ -44,6 +38,25 @@ namespace Engine::Abstract
 				m_components_[typeid(T).name()].insert(boost::reinterpret_pointer_cast<Component>(component));
 				m_priority_sorted_.insert(boost::reinterpret_pointer_cast<Component>(component));
 
+				UINT idx = 0;
+
+				while (true)
+				{
+					if (idx == g_invalid_id)
+					{
+						throw std::exception("Component ID overflow");
+					}
+
+					if (!m_assigned_component_ids_.contains(idx))
+					{
+						component->SetLocalID(idx);
+						m_assigned_component_ids_.insert(idx);
+						break;
+					}
+
+					idx++;
+				}
+
 				if (const auto scene = GetScene().lock())
 				{
 					scene->AddCacheComponent(component);
@@ -64,6 +77,25 @@ namespace Engine::Abstract
 				const auto& comp_set = m_components_[typeid(T).name()];
 
 				return boost::reinterpret_pointer_cast<T>(*comp_set.begin());
+			}
+
+			return {};
+		}
+
+		template <typename T>
+		boost::weak_ptr<T> GetComponentByLocal(ComponentID id)
+		{
+			if (m_assigned_component_ids_.contains(id))
+			{
+				const auto it = std::find_if(m_priority_sorted_.begin(), m_priority_sorted_.end(), [id](const auto& comp)
+				{
+					return comp.lock()->GetLocalID() == id;
+				});
+
+				if (it != m_priority_sorted_.end())
+				{
+					return boost::reinterpret_pointer_cast<T>((*it).lock());
+				}
 			}
 
 			return {};
@@ -142,18 +174,7 @@ namespace Engine::Abstract
 					}
 
 					const auto first = *comp_set.begin();
-					m_priority_sorted_.erase(first);
-					m_components_[typeid(T).name()].erase(first);
-
-					if (const auto scene = GetScene().lock())
-					{
-						scene->RemoveCacheComponent(first);
-					}
-
-					if (comp_set.empty())
-					{
-						m_components_.erase(typeid(T).name());
-					}
+					RemoveComponentCommon<T>(first);
 				}
 			}
 		}
@@ -182,18 +203,7 @@ namespace Engine::Abstract
 						return;
 					}
 
-					m_priority_sorted_.erase(found);
-					m_components_[typeid(T).name()].erase(found);
-
-					if (const auto scene = GetScene().lock())
-					{
-						scene->RemoveCacheComponent(*found);
-					}
-
-					if (comp_set.empty())
-					{
-						m_components_.erase(typeid(T).name());
-					}
+					RemoveComponentCommon<T>(*found);
 				}
 			}
 		}
@@ -250,7 +260,10 @@ namespace Engine::Abstract
 		bool GetActive() const { return m_active_; }
 
 	protected:
-		Object() : Actor(), m_active_(true), m_culled_(true) {};
+		Object() : Actor(), m_active_(true), m_culled_(true)
+		{
+		};
+
 		void AfterDeserialized() override;
 
 	public:
@@ -261,17 +274,38 @@ namespace Engine::Abstract
 		void FixedUpdate(const float& dt) override;
 
 	private:
+		SERIALIZER_ACCESS
+
+		friend class Engine::Scene;
+		template <typename T>
+		void RemoveComponentCommon(StrongComponent& value)
+		{
+			m_priority_sorted_.erase(value);
+			m_components_[typeid(T).name()].erase(value);
+			m_assigned_component_ids_.erase(value->GetLocalID());
+
+			if (const auto scene = GetScene().lock())
+			{
+				scene->RemoveCacheComponent(value);
+			}
+
+			if (m_components_[typeid(T).name()].empty())
+			{
+				m_components_.erase(typeid(T).name());
+			}
+		}
+
 		virtual void OnCollisionEnter(const Engine::Component::Collider& other);
 		virtual void OnCollisionContinue(const Engine::Component::Collider& other);
 		virtual void OnCollisionExit(const Engine::Component::Collider& other);
 
-		friend class boost::serialization::access;
 		bool m_active_ = true;
 		bool m_culled_ = true;
 
 		std::map<const std::string, std::set<StrongComponent, ComponentPriorityComparer>> m_components_;
 
 		// Non-serialized
+		std::set<ComponentID> m_assigned_component_ids_;
 		std::set<WeakComponent, ComponentPriorityComparer> m_priority_sorted_;
 		std::set<WeakResource, ResourcePriorityComparer> m_resources_;
 	};
