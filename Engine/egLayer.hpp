f1bc0e74416a1729b8a47249521502c0c29206d6
Colibri Shin <colibrishin@gmail.com>
2023-12-03 18:44:44 +0900

    layer: store object without id, prep for serialization

--- Engine/egLayer.hpp
+++ Engine/egLayer.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "egCommon.hpp"
-#include "egObject.hpp"
 #include "egRenderable.hpp"
 
 namespace Engine
@@ -18,55 +17,24 @@ namespace Engine
 		void Render(const float dt) override;
 		void FixedUpdate(const float& dt) override;
 
-		void AddGameObject(const StrongObject& obj)
-		{
-			if(m_objects_.contains(obj->GetID()))
-			{
-				return;
-			}
-
-			m_objects_.insert_or_assign(obj->GetID(), obj);
-			m_weak_objects_cache_.push_back(obj);
-		}
-
-		void RemoveGameObject(EntityID id)
-		{
-			if (m_objects_.contains(id))
-			{
-				m_objects_.erase(id);
-				std::erase_if(m_weak_objects_cache_, [id](const auto& obj) { return obj.lock()->GetID() == id; });
-			}
-		}
-
-		WeakObject GetGameObject (EntityID id) const
-		{
-			for (const auto& object : m_objects_)
-			{
-				if(object.first == id)
-				{
-					return object.second;
-				}
-			}
-
-			return {};
-		}
-
-		const std::vector<WeakObject>& GetGameObjects()
-		{
-			return m_weak_objects_cache_;
-		}
+		void AddGameObject(const StrongObject& obj);
+		void RemoveGameObject(EntityID id);
+		WeakObject GetGameObject (EntityID id) const;
+		const std::vector<WeakObject>& GetGameObjects();
 
 	protected:
 		void AfterDeserialized() override;
 
 	private:
-		friend class boost::serialization::access;
+		Layer() : m_layer_type_(eLayerType::LAYER_NONE) {}
+		SERIALIZER_ACCESS
 
 		eLayerType m_layer_type_;
-		std::map<const EntityID, StrongObject> m_objects_;
+		std::set<StrongObject> m_objects_;
 
 		// Non-serialized
-		std::vector<WeakObject> m_weak_objects_cache_;
+		std::vector<WeakObject> m_weak_objects_; // back-compatibility
+		std::map<const EntityID, WeakObject> m_weak_objects_cache_;
 
 	};
 }
