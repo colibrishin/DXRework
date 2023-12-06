1b775f609cd6d103b33adbb57436a345f9cd106d
Colibri Shin <colibrishin@gmail.com>
2023-12-06 19:48:57 +0900

    misc: resize window

--- Engine/egCommon.hpp
+++ Engine/egCommon.hpp
@@ -71,8 +71,8 @@ namespace Engine
 
 	inline std::atomic<bool> g_full_screen = false;
 	inline std::atomic<bool> g_vsync_enabled = true;
-	inline std::atomic<UINT> g_window_width = 800;
-	inline std::atomic<UINT> g_window_height = 600;
+	inline std::atomic<UINT> g_window_width = 1920;
+	inline std::atomic<UINT> g_window_height = 1080;
 
 	inline std::atomic<float> g_screen_near = 0.0001f;
 	inline std::atomic<float> g_screen_far = 1000.0f;
