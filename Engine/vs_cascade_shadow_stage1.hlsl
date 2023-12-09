2a644501338dd48d704f64e97723dd64086a5898
Colibri Shin <colibrishin@gmail.com>
2023-12-10 00:51:29 +0900

    shadow/shader: give vertex in world position

--- Engine/vs_cascade_shadow_stage1.hlsl
+++ Engine/vs_cascade_shadow_stage1.hlsl
@@ -8,10 +8,7 @@ GeometryShadowInputType main(VertexInputType input)
 
 	output.position = float4(input.position, 1.f);
 
-	// Calculate the position of the vertex against the world, view, and projection matrices.
 	output.position = mul(output.position, world);
-	output.position = mul(output.position, cam_view);
-	output.position = mul(output.position, cam_projection);
 
 	// Store the input color for the pixel shader to use.
 	output.tex = input.tex;
