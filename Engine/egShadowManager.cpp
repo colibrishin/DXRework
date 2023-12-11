fa79711d20d48e102ce86b800d13006822c17203
Colibri Shin <colibrishin@gmail.com>
2023-12-11 19:43:43 +0900

    shadowManager: build cascade shadow buffer, render mapping
    using bounding frustum for less complication

--- Engine/egShadowManager.cpp
+++ Engine/egShadowManager.cpp
@@ -9,6 +9,7 @@
 #include "egTransform.hpp"
 #include "egMesh.hpp"
 #include "egCamera.hpp"
+#include "egLight.hpp"
 #include "egProjectionFrustum.hpp"
 
 namespace Engine::Manager::Graphics
@@ -18,9 +19,6 @@ namespace Engine::Manager::Graphics
 		m_vs_stage1 = GetResourceManager().GetResource<Graphic::VertexShader>("vs_cascade_shadow_stage1").lock();
 		m_gs_stage1 = GetResourceManager().GetResource<Graphic::GeometryShader>("gs_cascade_shadow_stage1").lock();
 		m_ps_stage1 = GetResourceManager().GetResource<Graphic::PixelShader>("ps_cascade_shadow_stage1").lock();
-
-		m_vs_stage2 = GetResourceManager().GetResource<Graphic::VertexShader>("vs_cascade_shadow_stage2").lock();
-		m_ps_stage2 = GetResourceManager().GetResource<Graphic::PixelShader>("ps_cascade_shadow_stage2").lock();
 	}
 
 	void ShadowManager::PreUpdate(const float& dt)
@@ -33,33 +31,48 @@ namespace Engine::Manager::Graphics
 
 	void ShadowManager::PreRender(const float& dt)
 	{
-	}
+		GetRenderPipeline().TargetShadowMap();
 
-	void ShadowManager::Render(const float& dt)
-	{
+		m_vs_stage1->Render(placeholder);
+		m_gs_stage1->Render(placeholder);
+		m_ps_stage1->Render(placeholder);
+
 		if (const auto scene = GetSceneManager().GetActiveScene().lock())
 		{
 			const auto& lights = scene->GetGameObjects((eLayerType)LAYER_LIGHT);
 
+			const auto camera = scene->GetMainCamera().lock();
+
 			for (const auto& light : lights)
 			{
-				if (const auto locked = light.lock())
+				if (const auto locked = light.lock()->GetSharedPtr<Objects::Light>())
 				{
-					GetRenderPipeline().TargetShadowMap();
+					const auto tr = locked->GetComponent<Engine::Component::Transform>().lock();
 
-					locked->Render(0.f);
+					Vector3 light_dir;
+					(tr->GetPosition()).Normalize(light_dir);
+
+					SetCascadeShadow(light_dir);
 					BuildShadowMap(*scene);
-
-					GetRenderPipeline().ResetRenderTarget();
-					GetRenderPipeline().BindShadowMap();
-
-					ApplyShadow(*scene);
-
-					GetRenderPipeline().UnbindShadowMap();
-					GetRenderPipeline().ResetShadowMap();
 				}
 			}
 		}
+		
+		GetRenderPipeline().ResetShaders();
+		GetRenderPipeline().ResetRenderTarget();
+
+		// Pass #2 : Render scene with shadow map
+
+		GetRenderPipeline().BindShadowMap();
+		GetRenderPipeline().BindShadowSampler();
+	}
+
+	void ShadowManager::Render(const float& dt)
+	{
+		// Pass 2 Ends, reset shadow map
+
+		GetRenderPipeline().UnbindShadowMap();
+		GetRenderPipeline().ResetShadowMap();
 	}
 
 	void ShadowManager::FixedUpdate(const float& dt)
@@ -68,18 +81,10 @@ namespace Engine::Manager::Graphics
 
 	void ShadowManager::BuildShadowMap(Scene& scene) const
 	{
-		// no need to use delta time
-		constexpr float placeholder = 0.f;
-
-		// set up the shaders for stage 1
-		m_vs_stage1->Render(placeholder);
-		m_gs_stage1->Render(placeholder);
-		m_ps_stage1->Render(placeholder);
-
 		for (int i = 0; i < LAYER_MAX; ++i)
 		{
-			if (i == LAYER_LIGHT || i == LAYER_UI || i == LAYER_CAMERA) continue;
-
+			if (i == LAYER_LIGHT || i == LAYER_UI || i == LAYER_CAMERA || i == LAYER_SKYBOX) continue;
+		
 			for (const auto& objects : scene.GetGameObjects((eLayerType)i))
 			{
 				if (const auto locked = objects.lock())
@@ -95,47 +100,9 @@ namespace Engine::Manager::Graphics
 				}
 			}
 		}
-
-		GetRenderPipeline().ResetShaders();
 	}
 
-	void ShadowManager::ApplyShadow(Scene& scene) const
-	{
-		// no need to use delta time
-		constexpr float placeholder = 0.f;
-
-		// set up the shaders for stage 2
-		m_vs_stage2->Render(placeholder);
-		m_ps_stage2->Render(placeholder);
-
-		GetRenderPipeline().BindShadowSampler();
-
-		for (int i = 0; i < LAYER_MAX; ++i)
-		{
-			if (i == LAYER_LIGHT || i == LAYER_UI || i == LAYER_CAMERA) continue;
-
-			for (const auto& objects : scene.GetGameObjects((eLayerType)i))
-			{
-				if (const auto locked = objects.lock())
-				{
-					const auto tr = locked->GetComponent<Engine::Component::Transform>().lock();
-					const auto mesh = locked->GetResource<Engine::Resources::Mesh>().lock();
-
-					if (tr && mesh)
-					{
-						GetRenderPipeline().SetWorldMatrix(tr->m_transform_buffer_, SHADER_PIXEL);
-						tr->Render(placeholder);
-						mesh->Render(placeholder);
-					}
-				}
-			}
-		}
-
-		GetRenderPipeline().ResetSampler(SHADER_PIXEL);
-		GetRenderPipeline().ResetShaders();
-	}
-
-	void ShadowManager::CreateFrusta(const Matrix& projection, float start, float end, Vector4 cornerPoints[8]) const
+	void ShadowManager::CreateFrusta(const Matrix& projection, float start, float end, Subfrusta& subfrusta) const
 	{
 		BoundingFrustum frustum (projection);
 
@@ -155,18 +122,18 @@ namespace Engine::Manager::Graphics
 		const Vector4 LeftBottomNear = leftBottomV * nearV;
 		const Vector4 LeftBottomFar = leftBottomV * farV;
 
-		cornerPoints[0] = rightTopNear;
-		cornerPoints[1] = XMVectorSelect(rightTopNear, LeftBottomNear, vGrabX);
-		cornerPoints[2] = LeftBottomNear;
-		cornerPoints[3] = XMVectorSelect(rightTopNear, LeftBottomNear, vGrabY);
+		subfrusta.corners[0] = rightTopNear;
+		subfrusta.corners[1] = XMVectorSelect(rightTopNear, LeftBottomNear, vGrabX);
+		subfrusta.corners[2] = LeftBottomNear;
+		subfrusta.corners[3] = XMVectorSelect(rightTopNear, LeftBottomNear, vGrabY);
 
-		cornerPoints[4] = righTopFar;
-		cornerPoints[5] = XMVectorSelect(righTopFar, LeftBottomFar, vGrabX);
-		cornerPoints[6] = LeftBottomFar;
-		cornerPoints[7] = XMVectorSelect(righTopFar, LeftBottomFar, vGrabY);
+		subfrusta.corners[4] = righTopFar;
+		subfrusta.corners[5] = XMVectorSelect(righTopFar, LeftBottomFar, vGrabX);
+		subfrusta.corners[6] = LeftBottomFar;
+		subfrusta.corners[7] = XMVectorSelect(righTopFar, LeftBottomFar, vGrabY);
 	}
 
-	void ShadowManager::GetCascadeShadow(const Vector3& light_dir, Vector4 position[3], Matrix view[3], Matrix projection[3], Vector4 clip[3]) const
+	void ShadowManager::SetCascadeShadow(const Vector3& light_dir)
 	{
 		// https://cutecatgame.tistory.com/6
 
@@ -179,7 +146,7 @@ namespace Engine::Manager::Graphics
 
 				const float cascadeEnds[]
 				{
-					near_plane, 6.f, 18.f, far_plane
+					near_plane, 10.f, 80.f, far_plane
 				};
 
 				// for cascade shadow mapping, total 3 parts are used.
@@ -187,18 +154,17 @@ namespace Engine::Manager::Graphics
 				for (auto i = 0; i < g_max_shadow_cascades; ++i)
 				{
 					// frustum = near points 4 + far points 4
-					Vector4 current_corner[8]{};
 					CreateFrusta(
 						camera->GetProjectionMatrix(),
 						cascadeEnds[i], 
 						cascadeEnds[i + 1], 
-						current_corner);
+						m_subfrusta_[i]);
 
 					const auto view_inv = camera->GetViewMatrix().Invert();
 
 					// transform to world space
 					Vector4 center{};
-					for (auto& corner : current_corner)
+					for (auto& corner : m_subfrusta_[i].corners)
 					{
 						corner = Vector4::Transform(corner, view_inv);
 						center += corner;
@@ -208,7 +174,7 @@ namespace Engine::Manager::Graphics
 					center /= 8.f;
 
 					float radius = 0.f;
-					for (const auto& corner : current_corner)
+					for (const auto& corner : m_subfrusta_[i].corners)
 					{
 						float distance = Vector4::Distance(center, corner);
 						radius = std::max(radius, distance);
@@ -218,18 +184,32 @@ namespace Engine::Manager::Graphics
 
 					Vector3 maxExtent = Vector3{radius, radius, radius};
 					Vector3 minExtent = -maxExtent;
-
-					position[i] = center + (light_dir * minExtent.z);
-
-					view[i] = XMMatrixLookAtLH(position[i], Vector3(center), Vector3::Up);
-
 					const Vector3 cascadeExtents = maxExtent - minExtent;
 
-					projection[i] = XMMatrixOrthographicOffCenterLH(minExtent.x, maxExtent.x, minExtent.y, maxExtent.y, 0.f, cascadeExtents.z);
+					const auto pos = center + (light_dir * std::fabsf(minExtent.z));
 
-					clip[i] = Vector4{0.f, 0.f, cascadeEnds[i + 1], 1.f};
-					clip[i] = Vector4::Transform(clip[i], camera->GetProjectionMatrix()); // use z axis
+					// DX11 uses column major matrix
+
+					m_shadow_buffer_.view[i] =
+						XMMatrixTranspose(XMMatrixLookAtLH(
+						pos, 
+						Vector3(center), 
+						Vector3::Up));
+
+					m_shadow_buffer_.proj[i] =
+						XMMatrixTranspose(XMMatrixOrthographicOffCenterLH(
+							minExtent.x, 
+							maxExtent.x, 
+							minExtent.y, 
+							maxExtent.y, 
+							0.f, 
+							cascadeExtents.z));
+
+					m_shadow_buffer_.end_clip_spaces[i] = Vector4{0.f, 0.f, cascadeEnds[i + 1], 1.f};
+					m_shadow_buffer_.end_clip_spaces[i] = Vector4::Transform(m_shadow_buffer_.end_clip_spaces[i], camera->GetProjectionMatrix()); // use z axis
 				}
+
+				GetRenderPipeline().SetShadow(m_shadow_buffer_);
 			}
 		}
 	}
--- Engine/egShadowManager.hpp
+++ Engine/egShadowManager.hpp
@@ -3,8 +3,16 @@
 
 namespace Engine::Manager::Graphics
 {
+	constexpr float placeholder = 0.f;
+
 	class ShadowManager : public Abstract::Singleton<ShadowManager>
 	{
+	private:
+		struct Subfrusta
+		{
+			Vector4 corners[8];
+		};
+
 	public:
 		ShadowManager(SINGLETON_LOCK_TOKEN) : Abstract::Singleton<ShadowManager>() {}
 		void Initialize() override;
@@ -14,19 +22,18 @@ namespace Engine::Manager::Graphics
 		void Render(const float& dt) override;
 		void FixedUpdate(const float& dt) override;
 
-		void GetCascadeShadow(const Vector3& light_dir, Vector4 position[3], Matrix view[3], Matrix projection[3], Vector4 clip[3]) const;
+		void SetCascadeShadow(const Vector3& light_dir);
 
 	private:
-		void CreateFrusta(const Matrix& projection, float start, float end, Vector4 cornerPoints[]) const;
+		void CreateFrusta(const Matrix& projection, float start, float end, Subfrusta& subfrusta) const;
 
 		void BuildShadowMap(Scene& scene) const;
-		void ApplyShadow(Scene& scene) const;
 
 		boost::shared_ptr<Graphic::VertexShader> m_vs_stage1;
 		boost::shared_ptr<Graphic::GeometryShader> m_gs_stage1;
 		boost::shared_ptr<Graphic::PixelShader> m_ps_stage1;
 
-		boost::shared_ptr<Graphic::VertexShader> m_vs_stage2;
-		boost::shared_ptr<Graphic::PixelShader> m_ps_stage2;
+		Subfrusta m_subfrusta_[3];
+		CascadeShadowBuffer m_shadow_buffer_;
 	};
 }
