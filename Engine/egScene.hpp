9e5023645401c71ad96c90ba78ce862ee3a085fa
Colibri Shin <colibrishin@gmail.com>
2023-12-18 11:53:25 +0900

    scene: unwrap weak ptr, use type check, more strict parameter input

--- Engine/egScene.hpp
+++ Engine/egScene.hpp
@@ -3,6 +3,7 @@
 
 #include <boost/serialization/export.hpp>
 #include "egRenderable.h"
+#include "egLayer.h"
 #include "../octree/octree.h"
 
 namespace Engine
@@ -10,7 +11,6 @@ namespace Engine
     class Scene : public Abstract::Renderable
     {
     public:
-        Scene();
         Scene(const Scene& other) = default;
         ~Scene() override         = default;
 
@@ -27,8 +27,44 @@ namespace Engine
 
         void     OnDeserialized() override;
 
+        template <typename T, typename ObjLock = std::enable_if_t<std::is_base_of_v<Abstract::Object, T>>>
+        EntityID AddGameObject(const boost::shared_ptr<T>& obj, eLayerType layer)
+        {
+            m_layers[layer]->AddGameObject(obj);
+            m_cached_objects_.emplace(obj->GetID(), obj);
 
-        EntityID AddGameObject(const StrongObject& obj, eLayerType layer);
+            for (const auto& comp : obj->GetAllComponents())
+            {
+                m_cached_components_[comp.lock()->GetComponentType()].emplace(comp);
+            }
+
+            obj->template GetSharedPtr<Abstract::Actor>()->SetScene(GetSharedPtr<Scene>());
+            obj->template GetSharedPtr<Abstract::Actor>()->SetLayer(layer);
+
+            AssignLocalIDToObject(obj);
+
+            if (const auto tr = obj->template GetComponent<Components::Transform>().lock())
+            {
+                UpdatePosition(obj);
+            }
+
+            if (layer == LAYER_LIGHT && !std::is_base_of_v<Objects::Light, T>)
+            {
+                static_assert("Only light object can be added to light layer");
+            }
+            else if (layer == LAYER_CAMERA && !std::is_base_of_v<Objects::Camera, T>)
+            {
+                static_assert("Only camera object can be added to camera layer");
+            }
+
+            if constexpr (std::is_base_of_v<Objects::Light, T>)
+            {
+                RegisterLightToManager(obj);
+            }
+
+            return obj->GetID();
+        }
+
         void     RemoveGameObject(EntityID id, eLayerType layer);
 
         std::vector<WeakObject> GetGameObjects(eLayerType layer);
@@ -58,14 +94,31 @@ namespace Engine
             return m_layers.end();
         }
 
-        void AddCacheComponent(const WeakComponent& component);
-        void RemoveCacheComponent(const WeakComponent& component);
+        template <typename T, typename CompLock = std::enable_if_t<std::is_base_of_v<Abstract::Component, T>>>
+        void AddCacheComponent(const boost::shared_ptr<T>& component)
+        {
+            if (m_cached_objects_.contains(component->GetOwner().lock()->GetID()))
+            {
+                m_cached_components_[which_component<T>::value].insert(component);
+            }
+        }
+
+        template <typename T, typename CompLock = std::enable_if_t<std::is_base_of_v<Abstract::Component, T>>>
+        void RemoveCacheComponent(const boost::shared_ptr<T>& component)
+        {
+            if (m_cached_objects_.contains(component->GetOwner().lock()->GetID()))
+            {
+                m_cached_components_[which_component<T>::value].erase(component);
+            }
+        }
 
         template <typename T>
         const std::set<WeakComponent, ComponentPriorityComparer>& GetCachedComponents()
         {
-            return m_cached_components_[typeid(T).name()];
+            return m_cached_components_[which_component<T>::value];
         }
+
+        eSceneType GetType() const;
 
         void UpdatePosition(const WeakObject& obj);
         void GetNearestObjects(const Vector3& pos, std::vector<WeakObject>& out);
@@ -77,6 +130,9 @@ namespace Engine
             std::set<WeakObject, WeakComparer<Abstract::Object>>& out,
             int                                                   exhaust = 100);
 
+    protected:
+        Scene();
+
     private:
         SERIALIZER_ACCESS
 
@@ -84,8 +140,8 @@ namespace Engine
         void OpenLoadPopup(bool& is_load_open);
 
         void AssignLocalIDToObject(const StrongObject& obj);
-        void RegisterLightToManager(const StrongObject& obj);
-        void UnregisterLightFromManager(const WeakObject& obj);
+        void RegisterLightToManager(const StrongLight & obj);
+        void UnregisterLightFromManager(const StrongLight & obj);
 
         void RemoveObjectFromCache(const WeakObject& obj);
         void RemoveObjectFromOctree(const WeakObject& obj);
@@ -94,6 +150,7 @@ namespace Engine
 
         ActorID                           m_main_camera_local_id_;
         std::map<eLayerType, StrongLayer> m_layers;
+        eSceneType                        m_type_;
 
         // Non-serialized
         WeakObject m_observer_;
@@ -103,7 +160,7 @@ namespace Engine
 
         std::set<ActorID>              m_assigned_actor_ids_;
         std::map<EntityID, WeakObject> m_cached_objects_;
-        std::map<const std::string,
+        std::map<eComponentType,
                  std::set<WeakComponent, ComponentPriorityComparer>>
         m_cached_components_;
         Octree<std::set<WeakObject, WeakComparer<Abstract::Object>>>
