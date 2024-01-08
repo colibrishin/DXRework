34bab491014748464c292869e36dbeef5d409ff9
Colibri Shin <colibrishin@gmail.com>
2024-01-09 03:58:31 +0900

    refactor/shadowManager: use local value for evaluation and use sb

--- Engine/egShadowManager.hpp
+++ Engine/egShadowManager.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "egManager.hpp"
+#include "egStructuredBuffer.hpp"
 
 namespace Engine::Manager::Graphics
 {
@@ -41,15 +42,12 @@ namespace Engine::Manager::Graphics
 
         void InitializeShadowBuffer(DXPacked::ShadowVPResource& buffer);
         void BuildShadowMap(Scene& scene, const float dt) const;
-        void ClearShadowVP();
         void ClearShadowMaps();
 
         void CreateSubfrusta(
             const Matrix& projection, float start, float end,
             Subfrusta&    subfrusta) const;
-        void EvalShadowVP(
-            const Vector3& light_dir, CBs::ShadowVPCB&        buffer,
-            UINT           light_index, CBs::ShadowVPChunkCB& chunk);
+        void EvalShadowVP(const Vector3& light_dir, SBs::LightVPSB& buffer);
 
         StrongMaterial m_shadow_shaders_;
 
@@ -57,20 +55,15 @@ namespace Engine::Manager::Graphics
         Subfrusta m_subfrusta_[3];
 
         // lights from current scene
-        std::set<WeakLight, WeakComparer<Objects::Light>> m_lights_;
-
-        // light constant buffer
-        CBs::LightCB m_light_buffer_{};
+        std::map<LocalActorID, WeakLight> m_lights_;
 
         // The DX resources for each of the shadow map (texture, depth stencil view and shader resource view)
-        std::map<WeakLight, DXPacked::ShadowVPResource, WeakComparer<Objects::Light>>
-        m_dx_resource_shadow_vps_;
-        // The constant buffer for each of the shadow map
-        std::map<WeakLight, CBs::ShadowVPCB, WeakComparer<Objects::Light>>
-        m_cb_shadow_vps_;
+        std::map<LocalActorID, DXPacked::ShadowVPResource> m_dx_resource_shadow_vps_;
 
-        // The constant buffer for the chunk of shadow map
-        CBs::ShadowVPChunkCB m_cb_shadow_vps_chunk_;
+        // light structured buffer
+        StructuredBuffer<SBs::LightSB> m_sb_light_buffer_{};
+        // The structured buffer for the chunk of light view projection matrices
+        StructuredBuffer<SBs::LightVPSB> m_sb_light_vps_buffer_;
 
         D3D11_VIEWPORT m_viewport_;
 
