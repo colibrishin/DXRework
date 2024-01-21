069e90e051a694185bde6d48a60dbb66a1419414
Colibri Shin <colibrishin@gmail.com>
2024-01-22 00:36:03 +0900

    refactor/renderer: preprocess instancing, refactor render seqence

--- Engine/Renderer.cpp
+++ Engine/Renderer.cpp
@@ -15,110 +15,211 @@
 
 namespace Engine::Manager::Graphics
 {
-  void Renderer::PreUpdate(const float& dt) {}
+  void Renderer::PreUpdate(const float& dt)
+  {
+    m_normal_instance_map_.clear();
+    m_delayed_instance_map_.clear();
+    m_skinned_instance_map_.clear();
+    m_animation_texture_map_.clear();
+    m_b_ready_ = false;
+  }
 
   void Renderer::Update(const float& dt) {}
 
   void Renderer::FixedUpdate(const float& dt) {}
 
-  void Renderer::PreRender(const float& dt) {}
-
-  void Renderer::RenderModel(
-    const float& dt, const WeakModelRenderer& ptr_mr, const WeakTransform& ptr_tr, const WeakAnimator& ptr_atr
-  )
+  void Renderer::PreRender(const float& dt)
   {
-    const auto mr = ptr_mr.lock();
-    const auto tr = ptr_tr.lock();
-
-    const auto& ptr_model = mr->GetModel();
-    const auto& ptr_mtr   = mr->GetMaterial();
-
-    if (ptr_model.expired()) { return; }
-    if (ptr_mtr.expired()) { return; }
-
-    const auto& model      = ptr_model.lock();
-    const auto& mtr        = ptr_mtr.lock();
-    float       anim_frame = dt;
-
-    if (const auto atr = ptr_atr.lock(); atr && atr->GetActive())
+    if (const auto scene = GetSceneManager().GetActiveScene().lock())
     {
-      anim_frame = atr->GetFrame();
+      const auto mrs = scene->GetCachedComponents<Components::ModelRenderer>();
 
-      if (const auto anim = mtr->GetResource<Resources::BoneAnimation>(atr->GetAnimation()).lock())
+      for (const auto& ptr_mr : mrs)
       {
-        anim->PreRender(anim_frame);
-        anim->Render(anim_frame);
+        if (ptr_mr.expired()) { continue; }
+
+        // Retrieve the only object that has the model renderer, if object has the transform information,
+        // then add to the instance transformation map. (Object have eventually transform information,
+        // if the model renderer is attached to the object, however, it is checked for safe guard.)
+        const auto& mr = ptr_mr.lock()->GetSharedPtr<Components::ModelRenderer>();
+        const auto& mtr = mr->GetMaterial().lock();
+        const auto& obj = mr->GetOwner().lock();
+        const auto& tr = obj->GetComponent<Components::Transform>().lock();
+
+        if (!mr->GetActive()) { continue; }
+        if (!obj->GetActive()) { continue; }
+        if (!tr->GetActive()) { continue; }
+        if (!mtr) { continue; }
+        if (!tr) { continue; }
+        if (!obj) { continue; }
+
+        const bool delayed_flag = (obj->GetObjectType() == DEF_OBJ_T_DELAY_OBJ);
+        auto& target = delayed_flag ? m_delayed_instance_map_ : m_normal_instance_map_;
+
+        // Discard the object if it is not in the frustum. Note that delayed objects are not pass-through.
+        if (!GetProjectionFrustum().CheckRender(obj)) { continue; }
+
+        // Load the transformation of object. If there is another object that has the same shape and material,
+        // then it will be added to the same map and instanced.
+        target[mtr][obj] = { tr->GetWorldMatrix().Transpose(), 0 };
+
+        // If there is animation, then prefetch and add to the animation transformation map.
+        // There could be different skinned version of the same material.
+        if (const auto atr = obj->GetComponent<Components::Animator>().lock())
+        {
+          target[mtr][obj].bone_flag = 1;
+
+          if (!atr->GetActive())
+          {
+            target[mtr][obj].bone_flag = 0;
+            continue;
+          }
+
+          const auto animation_string = atr->GetAnimation();
+
+          if (const auto anim = mtr->GetResource<Resources::BoneAnimation>(animation_string).lock())
+          {
+            auto transform = anim->GetFrameAnimation(atr->GetFrame());
+
+            if (transform.empty()) { continue; }
+
+            // Transpose for hlsl.
+            std::ranges::for_each(transform, [](auto& mat){ mat.Transpose(); });
+            m_skinned_instance_map_[mtr][obj] = transform;
+          }
+        }
+      }
+
+      UINT max_bone_count = 0;
+      UINT instance_count = 0;
+
+      for (const auto& [mtr, map] : m_skinned_instance_map_)
+      {
+        for (const auto& bones : map | std::views::values)
+        {
+          max_bone_count = std::max(max_bone_count, static_cast<UINT>(bones.size()));
+          instance_count++;
+        }
+
+        D3D11_TEXTURE2D_DESC desc;
+        desc.Width              = max_bone_count * sizeof(Matrix);
+        desc.Height             = instance_count;
+        desc.ArraySize          = 1;
+        desc.MipLevels          = -1;
+        desc.Format             = DXGI_FORMAT_R32G32B32A32_FLOAT; // one row size
+        desc.SampleDesc.Count   = 1;
+        desc.SampleDesc.Quality = 0;
+        desc.Usage              = D3D11_USAGE_DYNAMIC;
+        desc.BindFlags          = D3D11_BIND_SHADER_RESOURCE;
+        desc.CPUAccessFlags     = D3D11_CPU_ACCESS_WRITE;
+        desc.MiscFlags          = 0;
+
+        D3D11_SHADER_RESOURCE_VIEW_DESC srv_desc;
+        srv_desc.Format                    = desc.Format;
+        srv_desc.ViewDimension             = D3D11_SRV_DIMENSION_TEXTURE2D;
+        srv_desc.Texture2D.MostDetailedMip = 0;
+        srv_desc.Texture2D.MipLevels       = -1;
+
+        GetD3Device().CreateTexture2D
+          (
+           desc, srv_desc, m_animation_texture_map_[mtr].texture.ReleaseAndGetAddressOf(),
+           m_animation_texture_map_[mtr].srv.ReleaseAndGetAddressOf()
+          );
+
+        D3D11_MAPPED_SUBRESOURCE mapped{};
+        GetD3Device().GetContext()->Map(
+            m_animation_texture_map_[mtr].texture.Get(), 
+            0, 
+            D3D11_MAP_WRITE_DISCARD, 
+            0, 
+            &mapped);
+
+        UINT offset = 0;
+        for (const auto& bones : map | std::views::values)
+        {
+          std::memcpy(mapped.pData + offset, bones.data(), sizeof(Matrix) * bones.size());
+          offset += sizeof(Matrix) * bones.size();
+        }
+
+        GetD3Device().GetContext()->Unmap(m_animation_texture_map_[mtr].texture.Get(), 0);
       }
     }
-    else { mtr->IgnoreAnimation(true); }
 
-    Components::Transform::Bind(*tr);
+    m_b_ready_ = true;
+  }
 
-    mtr->PreRender(anim_frame);
-    mtr->Render(anim_frame);
-    model->PreRender(anim_frame);
-    model->Render(anim_frame);
+  void Renderer::RenderPass(const float& dt, bool post, bool shader)
+  {
+    if (!m_b_ready_) { throw std::logic_error("Render pass called before instance map initialization"); }
 
-    mtr->PostRender(anim_frame);
-    model->PostRender(anim_frame);
+    const auto& target = post ? m_delayed_instance_map_ : m_normal_instance_map_;
 
-    if (const auto atr = ptr_atr.lock(); atr && atr->GetActive())
+    // Create a structured buffer for instance information. (Animation and transformation)
+    UINT max_instance_count = 0;
+
+    for (const auto& map : target | std::views::values)
     {
-      if (const auto anim = mtr->GetResource<Resources::BoneAnimation>
-        (atr->GetAnimation()).lock()) { anim->PostRender(anim_frame); }
+      max_instance_count = std::max(max_instance_count, static_cast<UINT>(map.size()));
     }
-    else { mtr->IgnoreAnimation(false); }
+
+    StructuredBuffer<SBs::InstanceSB> instance_sb;
+    instance_sb.Create(max_instance_count, nullptr, true);
+
+    // Iterate through the map and render the shape.
+    for (const auto& [mtr_ptr, instance_map] : target)
+    {
+      // Collect the instance information of the current material.
+      std::vector<SBs::InstanceSB> instance_data;
+      const UINT                   instance_count = static_cast<UINT>(instance_map.size());
+
+      instance_data.reserve(instance_count);
+
+      for (const auto& transform : instance_map | std::views::values) { instance_data.push_back(transform); }
+
+      // Set and bind the instance data to vertex shader.
+      instance_sb.SetData(static_cast<UINT>(instance_data.size()), instance_data.data());
+      instance_sb.Bind(SHADER_VERTEX);
+
+      // Bind the bone texture.
+      if (m_animation_texture_map_.contains(mtr_ptr))
+      {
+        GetRenderPipeline().BindResource(
+          RESERVED_BONES, 
+          SHADER_VERTEX, 
+          m_animation_texture_map_.at(mtr_ptr).srv.GetAddressOf());
+      }
+
+      const auto& mtr = mtr_ptr.lock();
+
+      // This should not be happened.
+      if (!mtr) { throw std::logic_error("Material not found unexpectedly"); }
+
+      // Render the material within the instance count.
+      mtr->SetInstanceCount(instance_count);
+      if (!shader) { mtr->IgnoreShader(); }; // Ignore shader for shadow pass.
+
+      mtr->PreRender(dt);
+      mtr->Render(dt);
+      mtr->PostRender(dt);
+
+      // Unbind the instance data from vertex shader.
+      instance_sb.Unbind(SHADER_VERTEX);
+    }
+
+    GetRenderPipeline().UnbindResource(RESERVED_BONES, SHADER_VERTEX);
+    instance_sb.Clear();
   }
 
   void Renderer::Render(const float& dt)
   {
     const auto& scene = GetSceneManager().GetActiveScene().lock();
 
-    for (int i = 0; i < LAYER_MAX; ++i)
-    {
-      if (i == LAYER_UI) { GetReflectionEvaluator().RenderFinished(); }
+    RenderPass(dt, false);
 
-      for (const auto& object : *(*scene)[i])
-      {
-        if (!object->GetActive()) { continue; }
+    // Notify the reflection evaluator that the rendering is finished.
+    GetReflectionEvaluator().RenderFinished();
 
-        if (!GetProjectionFrustum().CheckRender(object)) { continue; }
-
-        if (object->GetObjectType() == DEF_OBJ_T_DELAY_OBJ)
-        {
-          m_delayed_objects_.push(object);
-          continue;
-        }
-
-        const auto  ptr_mr  = object->GetComponent<Components::ModelRenderer>();
-        const auto& ptr_tr  = object->GetComponent<Components::Transform>();
-        const auto& ptr_atr = object->GetComponent<Components::Animator>();
-
-        if (ptr_mr.expired()) { continue; }
-        if (ptr_tr.expired()) { continue; }
-
-        RenderModel(dt, ptr_mr, ptr_tr, ptr_atr);
-      }
-    }
-
-    while (!m_delayed_objects_.empty())
-    {
-      const auto ptr_object = m_delayed_objects_.front();
-      m_delayed_objects_.pop();
-
-      if (ptr_object.expired()) { continue; }
-
-      const auto object = ptr_object.lock();
-
-      const auto  ptr_mr  = object->GetComponent<Components::ModelRenderer>();
-      const auto& ptr_tr  = object->GetComponent<Components::Transform>();
-      const auto& ptr_atr = object->GetComponent<Components::Animator>();
-
-      if (ptr_mr.expired()) { continue; }
-      if (ptr_tr.expired()) { continue; }
-
-      RenderModel(dt, ptr_mr, ptr_tr, ptr_atr);
-    }
+    RenderPass(dt, true);
   }
 
   void Renderer::PostRender(const float& dt) {}
@@ -126,4 +227,6 @@ namespace Engine::Manager::Graphics
   void Renderer::PostUpdate(const float& dt) {}
 
   void Renderer::Initialize() {}
+
+  bool Renderer::Ready() const { return m_b_ready_; }
 }
--- Engine/Renderer.h
+++ Engine/Renderer.h
@@ -7,8 +7,9 @@ namespace Engine::Manager::Graphics
   class Renderer : public Abstract::Singleton<Renderer>
   {
   public:
-    Renderer(SINGLETON_LOCK_TOKEN)
-      : Singleton() {}
+    explicit Renderer(SINGLETON_LOCK_TOKEN)
+      : Singleton(),
+        m_b_ready_(false) {}
 
     void PreUpdate(const float& dt) override;
     void Update(const float& dt) override;
@@ -19,13 +20,28 @@ namespace Engine::Manager::Graphics
     void PostUpdate(const float& dt) override;
     void Initialize() override;
 
+    [[nodiscard]] bool Ready() const;
+    void RenderPass(const float & dt, bool post = false, bool shader = true);
+
   private:
+    using ObjectInstancingMap = std::map<WeakObject, SBs::InstanceSB, WeakComparer<Abstract::Object>>;
+    using SkinnedInstancingMap = std::map<WeakObject, std::vector<Matrix>, WeakComparer<Abstract::Object>>;
+
+    struct AnimationTexture
+    {
+      ComPtr<ID3D11Texture2D> texture;
+      ComPtr<ID3D11ShaderResourceView> srv;
+    };
+
     friend struct SingletonDeleter;
     ~Renderer() override = default;
 
-    void RenderModel(
-      const float& dt, const WeakModelRenderer& ptr_mr, const WeakTransform& ptr_tr, const WeakAnimator& ptr_atr
-    );
+    bool m_b_ready_;
+
+    std::map<WeakMaterial, ObjectInstancingMap, WeakComparer<Resources::Material>> m_normal_instance_map_;
+    std::map<WeakMaterial, SkinnedInstancingMap, WeakComparer<Resources::Material>> m_skinned_instance_map_;
+    std::map<WeakMaterial, ObjectInstancingMap, WeakComparer<Resources::Material>> m_delayed_instance_map_;
+    std::map<WeakMaterial, AnimationTexture, WeakComparer<Resources::Material>> m_animation_texture_map_;
 
     std::queue<WeakObject> m_delayed_objects_;
   };
