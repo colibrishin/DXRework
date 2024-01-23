c5135eca0831220dfa211034d29022b93b55f858
Colibri Shin <colibrishin@gmail.com>
2024-01-24 03:00:55 +0900

    refactor/reflectionManager: use texture class

--- Engine/egReflectionEvaluator.h
+++ Engine/egReflectionEvaluator.h
@@ -1,13 +1,32 @@
 #pragma once
 #include "pch.h"
 
+#include "egGlobal.h"
+#include "egTexture2D.h"
+
 namespace Engine::Manager::Graphics
 {
   class ReflectionEvaluator : public Abstract::Singleton<ReflectionEvaluator>
   {
   public:
     ReflectionEvaluator(SINGLETON_LOCK_TOKEN)
-      : Singleton() {}
+      : Singleton(),
+        m_copy_
+        (
+         "", {
+           .Width = g_window_width,
+           .Height = g_window_height,
+           .Depth = 0,
+           .ArraySize = 1,
+           .Format = DXGI_FORMAT_R8G8B8A8_UNORM,
+           .CPUAccessFlags = 0,
+           .BindFlags = D3D11_BIND_SHADER_RESOURCE,
+           .MipsLevel = 1,
+           .MiscFlags = 0,
+           .Usage = D3D11_USAGE_DEFAULT,
+           .SampleDesc = { .Count = 1, .Quality = 0 }
+         }
+        ) {}
 
     void PreUpdate(const float& dt) override;
     void Update(const float& dt) override;
@@ -24,6 +43,6 @@ namespace Engine::Manager::Graphics
     friend struct SingletonDeleter;
     ~ReflectionEvaluator() override = default;
 
-    DXPacked::RenderedResource m_rendered_buffer_;
+    Resources::Texture2D m_copy_; 
   };
 }
