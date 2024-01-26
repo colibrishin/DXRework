05312ff421664e8039a7483b8c055c2b22320ec0
Colibri Shin <colibrishin@gmail.com>
2024-01-26 22:48:52 +0900

    perf/shader: reduce local variables

--- Engine/cs_particle.hlsl
+++ Engine/cs_particle.hlsl
@@ -1,6 +1,7 @@
 #include "common.hlsli"
 
 #define PARAM_NUM_PARTICLE g_iParam[0].y
+#define PARAM_DT g_fParam[0].z
 
 [numthreads(32, 32, 1)]
 void cs_main(
@@ -9,30 +10,28 @@ void cs_main(
 {
   const uint flat_idx = dispatchThreadId.x + dispatchThreadId.y * 32;
 
+  // todo: thread safety for global variable.
   if (flat_idx >= PARAM_NUM_PARTICLE) { return; }
+    
+  float dt = PARAM_DT;
 
-  InstanceElement instance = uavInstance.Load(flat_idx);
-
-#define INST_DELTATIME  instance.fParam[0].x
-#define INST_LIFE       instance.fParam[0].y
-#define INST_VELOCITY   instance.vParam[0]
-#define INST_LOCAL      instance.mParam[0]
+#define INST_LIFE       uavInstance[flat_idx].fParam[0].y
+#define INST_VELOCITY   uavInstance[flat_idx].vParam[0]
+#define INST_LOCAL      uavInstance[flat_idx].mParam[0]
 
   float4 curr_pos = GetTranslation(INST_LOCAL);
-  curr_pos += INST_VELOCITY * INST_DELTATIME;
-  INST_LOCAL = matrix
-    (
-     INST_LOCAL._11, INST_LOCAL._12, INST_LOCAL._13, curr_pos.x,
-     INST_LOCAL._21, INST_LOCAL._22, INST_LOCAL._23, curr_pos.y,
-     INST_LOCAL._31, INST_LOCAL._32, INST_LOCAL._33, curr_pos.z,
-     INST_LOCAL._41, INST_LOCAL._42, INST_LOCAL._43, INST_LOCAL._44
-    );
-  INST_LIFE += INST_DELTATIME;
+  curr_pos += INST_VELOCITY * PARAM_DT;
 
-#undef INST_DELTATIME
+  matrix mat = INST_LOCAL;
+
+  mat._41 = curr_pos.x;
+  mat._42 = curr_pos.y;
+  mat._43 = curr_pos.z;
+
+  INST_LOCAL = mat;
+  INST_LIFE += PARAM_DT;
+
 #undef INST_LIFE
 #undef INST_VELOCITY
 #undef INST_LOCAL
-
-  uavInstance[flat_idx] = instance;
 }
