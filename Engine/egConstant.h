166c7a4bb2d82e7327b09acbd76822e87e4e284a
Colibri Shin <colibrishin@gmail.com>
2024-01-28 23:42:42 +0900

    tweak/debug/physics: lower update rate, widen map size

--- Engine/egConstant.h
+++ Engine/egConstant.h
@@ -23,8 +23,8 @@ namespace Engine
   // Physics Constants
   constexpr float   g_epsilon                 = 0.001f;
   constexpr float   g_gravity_acc             = 9.81f;
-  constexpr float   g_fixed_update_interval   = 0.02f;
-  constexpr float   g_lerp                    = 0.02f;
+  constexpr float   g_fixed_update_interval   = 0.01f;
+  constexpr float   g_lerp                    = 0.01f;
   constexpr Vector3 g_gravity_vec             = Vector3(0.0f, -g_gravity_acc, 0.0f);
   constexpr float   g_restitution_coefficient = 0.6f;
   constexpr float   g_drag_coefficient        = 0.25f;
@@ -38,14 +38,14 @@ namespace Engine
   constexpr int    g_debug_y_movement        = 15;
   constexpr int    g_debug_y_initial         = 0;
   constexpr float  g_debug_message_life_time = 1.0f;
-  constexpr size_t g_debug_message_max       = 200;
+  constexpr size_t g_debug_message_max       = 1000;
   constexpr bool   g_debug_observer          = false;
 
   // Misc
   constexpr LONG_PTR g_invalid_id               = -1;
   constexpr Vector3  g_forward                  = {0.f, 0.f, 1.f};
   constexpr Vector3  g_backward                 = {0.f, 0.f, -1.f};
-  constexpr size_t   g_max_map_size             = 128; // only in power of 2
+  constexpr size_t   g_max_map_size             = 2048; // only in power of 2
   constexpr size_t   g_octree_negative_round_up = g_max_map_size / 2;
 
   static_assert(g_max_map_size % 2 == 0, "Map size should be in the power of 2");
