d8e72a22b922781e8d2e69024bca0011782f5afe
Colibri Shin <colibrishin@gmail.com>
2024-02-03 14:10:45 +0900

    refactor/physics: velocity evaluation

--- Engine/egPhysics.hpp
+++ Engine/egPhysics.hpp
@@ -40,4 +40,19 @@ namespace Engine::Physics
   };
 
   extern Vector3 __vectorcall EvalGravity(float invMass, float dt);
+
+  __forceinline Vector3 __vectorcall EvalT1PositionDelta(
+    const Vector3& t0_vel, const Vector3& t0_force, const float dt
+  )
+  {
+    return (t0_vel * dt) + (t0_force * (dt * dt * 0.5f));
+  }
+
+  __forceinline Vector3 __vectorcall EvalT1Velocity(
+    const Vector3& t0_vel, const Vector3& t0_force, const Vector3& t1_force, const float dt
+  )
+  {
+    return t0_vel + ((t0_force + t1_force) * (0.5f * dt));
+  }
+
 } // namespace Engine::Physics
