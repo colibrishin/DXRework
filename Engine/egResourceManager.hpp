a7c851b600c63a4a6dfaccd8650da24a146e5aea
Colibri Shin <colibrishin@gmail.com>
2024-02-22 14:36:20 +0900

    fix/resourceManager: check raw path

--- Engine/egResourceManager.hpp
+++ Engine/egResourceManager.hpp
@@ -30,6 +30,7 @@ namespace Engine::Manager
     void AddResource(const boost::shared_ptr<T>& resource)
     {
       if (!resource->GetMetadataPath().empty() && GetResourceByMetadataPath<T>(resource->GetMetadataPath()).lock())  { return; }
+      if (!resource->GetPath().empty() && GetResourceByRawPath<T>(resource->GetPath()).lock()) { return; }
 
       m_resources_[which_resource<T>::value].insert(resource);
     }
@@ -37,10 +38,8 @@ namespace Engine::Manager
     template <typename T, typename ResLock = std::enable_if_t<std::is_base_of_v<Abstract::Resource, T>>>
     void AddResource(const EntityName& name, const boost::shared_ptr<T>& resource)
     {
-      if (!resource->GetMetadataPath().empty() && GetResourceByMetadataPath<T>(resource->GetMetadataPath()).lock())
-      {
-        return;
-      }
+      if (!resource->GetMetadataPath().empty() && GetResourceByMetadataPath<T>(resource->GetMetadataPath()).lock()) { return; }
+      if (!resource->GetPath().empty() && GetResourceByRawPath<T>(resource->GetPath()).lock()) { return; }
 
       m_resources_[which_resource<T>::value].insert(resource);
       resource->SetName(name);
