662037601493d7549b3692db285a2a9c4e2ea767
Colibri Shin <colibrishin@gmail.com>
2024-02-29 18:08:58 +0900

    refactor/resource: move serialization down to entity

--- Engine/egResource.cpp
+++ Engine/egResource.cpp
@@ -47,14 +47,7 @@ namespace Engine::Abstract
     ImGui::Indent(2);
     ImGui::Checkbox("Loaded", &m_bLoaded_);
     TextDisabled("Resource Path", m_path_str_);
-    TextDisabled("Metadata Path", m_meta_str_);
     ImGui::Unindent(2);
-
-    if (ImGui::Button("Save"))
-    {
-      // todo: future + promise (async)
-      serializeImpl();
-    }
   }
 
   Resource::Resource(std::filesystem::path path, eResourceType type)
@@ -68,6 +61,7 @@ namespace Engine::Abstract
 
   void Resource::OnDeserialized()
   {
+    Renderable::OnDeserialized();
     m_bLoaded_ = false;
     m_path_    = m_path_str_;
   }
