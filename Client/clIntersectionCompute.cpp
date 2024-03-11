13e5b6598e438d488f0b664373a6fc792a476dbe
Colibri Shin <colibrishin@gmail.com>
2024-03-11 22:07:32 +0900

    feat/intersectionCompute: initial value setters

--- Client/clIntersectionCompute.cpp
+++ Client/clIntersectionCompute.cpp
@@ -1,1 +1,65 @@
 #include "pch.h"
+#include "clIntersectionCompute.h"
+
+#include "egRenderPipeline.h"
+
+namespace Client::ComputeShaders
+{
+  IntersectionCompute::IntersectionCompute()
+    : ComputeShader("IntersectionCompute", "", {32, 32, 1}),
+      m_light_table_ptr_(nullptr),
+      m_target_light_(0) { }
+
+  void IntersectionCompute::OnImGui(const StrongParticleRenderer& pr) {}
+
+  void IntersectionCompute::preDispatch()
+  {
+    if (m_light_table_ptr_ == nullptr)
+    {
+      return;
+    }
+
+    m_light_table_ptr_->BindUAV();
+
+    // 512 x 512
+    SetGroup({32, 8, 1});
+
+    std::vector<ID3D11ShaderResourceView*> uavs;
+
+    for (const auto& tex : m_intersection_texture_)
+    {
+      uavs.push_back(tex->GetSRV());
+    }
+
+    GetRenderPipeline().BindResources
+      (
+       BIND_SLOT_UAV_TEXARR,
+       SHADER_COMPUTE,
+       uavs.data(),
+       static_cast<UINT>(uavs.size())
+      );
+
+    GetRenderPipeline().SetParam<int>(m_target_light_, target_light_slot);
+  }
+
+  void IntersectionCompute::postDispatch()
+  {
+    if (m_light_table_ptr_ == nullptr)
+    {
+      return;
+    }
+
+    m_light_table_ptr_->UnbindUAV();
+
+    GetRenderPipeline().UnbindResource(BIND_SLOT_UAV_TEXARR, SHADER_COMPUTE);
+    GetRenderPipeline().SetParam<int>(0, target_light_slot);
+
+    m_light_table_ptr_ = nullptr;
+    m_target_light_ = 0;
+    m_intersection_texture_.clear();
+  }
+
+  void IntersectionCompute::loadDerived() {}
+
+  void IntersectionCompute::unloadDerived() {}
+}
--- Client/clIntersectionCompute.h
+++ Client/clIntersectionCompute.h
@@ -3,14 +3,19 @@
 #include <egComputeShader.h>
 
 #include "Client.h"
+#include "clIntensityTexture.h"
+#include "egStructuredBuffer.hpp"
 
 namespace Client::ComputeShaders
 {
   class IntersectionCompute final : public Engine::Resources::ComputeShader
   {
   public:
+    constexpr static size_t target_light_slot = 1;
 
-    struct LightTable
+    IntersectionCompute();
+
+    struct LightTableSB
     {
       CLIENT_SB_UAV_T(CLIENT_SBUAV_TYPE_INTERSECTION)
 
@@ -19,11 +24,22 @@ namespace Client::ComputeShaders
 
     void OnImGui(const StrongParticleRenderer& pr) override;
 
+    void SetLightTable(Graphics::StructuredBuffer<LightTableSB>* light_table_ptr) { m_light_table_ptr_ = light_table_ptr; }
+    void SetIntersectionTexture(const std::vector<Client::Resource::IntensityTexture*>& texs) { m_intersection_texture_ = texs; }
+    void SetTargetLight(const UINT target_light) { m_target_light_ = target_light; }
+
+
   protected:
     void preDispatch() override;
     void postDispatch() override;
     void loadDerived() override;
     void unloadDerived() override;
+
+  private:
+    Graphics::StructuredBuffer<LightTableSB>* m_light_table_ptr_;
+    std::vector<Client::Resource::IntensityTexture*> m_intersection_texture_;
+    UINT m_target_light_;
+
   };
 }
 
