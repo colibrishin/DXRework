a5a5ee86b8e49d439bb9b4f111b69cff4a25be23
Colibri Shin <colibrishin@gmail.com>
2024-03-11 22:08:41 +0900

    fix/shader: dimension, value loaded in uint4

--- Client/cs_intensity_test.hlsl
+++ Client/cs_intensity_test.hlsl
@@ -7,34 +7,32 @@ struct LightTable
 
 RWStructuredBuffer<LightTable> g_lightTable : register(u7);
 
-#define TEXTURE_ARRAY texArr00
+#define TEXTURE_ARRAY uavArr00
 #define PARAM_NUM_LIGHTS g_iParam[0].x
 
 [maxthreads(32, 32, 1)]
 void cs_main(uint3 tId : SV_DispatchThreadID)
 {
 #define PARAM_TARGET_LIGHT g_iParam[0].y
-  // designated pixel (500 x 500, if shadow map size is not modified)
+  // designated pixel (512 x 512, if shadow map size is not modified)
 
   const uint group_size = 32 * 8;
   const uint flat_idx = tId.x + tId.y * group_size;
 
-  // 250,000 pixels
-  const uint width = 500;
-  const uint height = 500;
+  // ~250,000 pixels
+  const uint width  = 512;
+  const uint height = 512;
 
   if (flat_idx >= width * height) { return; }
 
-  float4 texel = TEXTURE_ARRAY.Load
+  const uint4 texel = TEXTURE_ARRAY.Load
   (
-    int4(PARAM_TARGET_LIGHT, flat_idx / 500, flat_idx % 500, 0)
+    int4(PARAM_TARGET_LIGHT, flat_idx / width, flat_idx % height, 0)
   );
-
-  const int availability = asint(texel.x);
 
   for (int j = 0; j < PARAM_NUM_LIGHTS; ++j)
   {
-    if (availability & (1 << j))
+    if (texel.x & (1 << j))
     {
       g_lightTable[0].table[PARAM_TARGET_LIGHT][j].x = 1;
     }
