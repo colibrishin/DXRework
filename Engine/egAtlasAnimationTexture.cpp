edbfc5154f62bafd150cdc1fe6f786151d780084
Colibri Shin <colibrishin@gmail.com>
2024-03-19 15:18:36 +0900

    fix/atlasTex: do not re-evaluate textures if path persists

--- Engine/egAtlasAnimationTexture.cpp
+++ Engine/egAtlasAnimationTexture.cpp
@@ -53,60 +53,66 @@ namespace Engine::Resources
 
   void AtlasAnimationTexture::loadDerived(ComPtr<ID3D11Resource>& res)
   {
-    // todo: double operation before and after the serialization.
-
     const UINT num_atlases = static_cast<UINT>(m_atlases_.size());
-    UINT width = 0, height = 0;
 
-    // Find the largest atlas to set the 3D texture dimensions.
-    for (UINT i = 0; i < num_atlases; ++i)
+    // Build the atlas from textures if no path is provided. (assuming that this has not been serialized before)
+    if (GetPath().empty())
     {
-      m_atlases_[i]->Load();
-      width = std::max(width, m_atlases_[i]->GetWidth());
-      height = std::max(height, m_atlases_[i]->GetHeight());
-    }
+      UINT width = 0, height = 0;
 
-    LazyDescription
-      (
-       {
-         .Width = width,
-         .Height = height,
-         .Depth = num_atlases,
-         .ArraySize = 1,
-         .Format = DXGI_FORMAT_B8G8R8A8_UNORM,
-         .CPUAccessFlags = 0,
-         .BindFlags = D3D11_BIND_SHADER_RESOURCE,
-         .MipsLevel = 1,
-         .MiscFlags = 0,
-         .Usage = D3D11_USAGE_DEFAULT,
-         .SampleDesc = {1, 0}
-       }
-      );
+      // Find the largest atlas to set the 3D texture dimensions.
+      for (UINT i = 0; i < num_atlases; ++i)
+      {
+        m_atlases_[i]->Load();
+        width  = std::max(width, m_atlases_[i]->GetWidth());
+        height = std::max(height, m_atlases_[i]->GetHeight());
+      }
+
+      LazyDescription
+        (
+         {
+           .Width = width,
+           .Height = height,
+           .Depth = num_atlases,
+           .ArraySize = 1,
+           .Format = DXGI_FORMAT_B8G8R8A8_UNORM,
+           .CPUAccessFlags = 0,
+           .BindFlags = D3D11_BIND_SHADER_RESOURCE,
+           .MipsLevel = 1,
+           .MiscFlags = 0,
+           .Usage = D3D11_USAGE_DEFAULT,
+           .SampleDesc = {1, 0}
+         }
+        );
+    }
 
     Texture3D::loadDerived(res);
 
-    ComPtr<ID3D11Texture3D> tex;
-    DX::ThrowIfFailed(res.As(&tex));
-
-    // Merge all the atlases into the 3D texture.
-    // Keep the atlases dimensions, progress from left to right, top to bottom
-    // for matching with xml data.
-    for (UINT i = 0; i < num_atlases; ++i)
+    if (GetPath().empty())
     {
-      ComPtr<ID3D11Texture2D> anim = m_atlases_[i]->As<ID3D11Texture2D>();
-      D3D11_BOX               box  = {0, 0, 0, m_atlases_[i]->GetWidth(), m_atlases_[i]->GetHeight(), 1};
+      ComPtr<ID3D11Texture3D> tex;
+      DX::ThrowIfFailed(res.As(&tex));
 
-      GetD3Device().GetContext()->CopySubresourceRegion
-        (
-         tex.Get(),
-         0,
-         0,
-         0,
-         i,
-         anim.Get(),
-         0,
-         &box
-        );
+      // Merge all the atlases into the 3D texture.
+      // Keep the atlases dimensions, progress from left to right, top to bottom
+      // for matching with xml data.
+      for (UINT i = 0; i < num_atlases; ++i)
+      {
+        ComPtr<ID3D11Texture2D> anim = m_atlases_[i]->As<ID3D11Texture2D>();
+        D3D11_BOX               box  = {0, 0, 0, m_atlases_[i]->GetWidth(), m_atlases_[i]->GetHeight(), 1};
+
+        GetD3Device().GetContext()->CopySubresourceRegion
+          (
+           tex.Get(),
+           0,
+           0,
+           0,
+           i,
+           anim.Get(),
+           0,
+           &box
+          );
+      }
     }
   }
 }
