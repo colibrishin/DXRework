8ff831b1158db15bd9fd7f758f91f05bbe780725
Colibri Shin <colibrishin@gmail.com>
2024-03-20 15:53:34 +0900

    fix/playerScript: remove prevState and state check

--- Client/clFezPlayerScript.cpp
+++ Client/clFezPlayerScript.cpp
@@ -53,36 +53,28 @@ namespace Client::Scripts
 
   void FezPlayerScript::Update(const float& dt)
   {
-    if (m_prev_state_ == m_state_)
+    switch (m_state_)
     {
-      switch (m_state_)
-      {
-      case CHAR_STATE_IDLE: 
-          UpdateMove();
-          UpdateRotate();
-          UpdateJump();
-          break;
-      case CHAR_STATE_WALK: 
-          UpdateMove();
-          UpdateJump();
-          break;
-      case CHAR_STATE_RUN: break;
-      case CHAR_STATE_JUMP: 
-          UpdateMove();
-          break;
-      case CHAR_STATE_CLIMB: break;
-      case CHAR_STATE_SWIM: break;
-      case CHAR_STATE_ROTATE:
-          UpdateRotate();
-          break;
-      case CHAR_STATE_FALL: 
-          break;
-      case CHAR_STATE_ATTACK: break;
-      case CHAR_STATE_HIT: break;
-      case CHAR_STATE_DIE: break;
-      case CHAR_STATE_MAX: break;
-      default: ;
-      }
+    case CHAR_STATE_IDLE: UpdateMove();
+      UpdateRotate();
+      UpdateJump();
+      break;
+    case CHAR_STATE_WALK: UpdateMove();
+      UpdateJump();
+      break;
+    case CHAR_STATE_RUN: break;
+    case CHAR_STATE_JUMP: UpdateMove();
+      break;
+    case CHAR_STATE_CLIMB: break;
+    case CHAR_STATE_SWIM: break;
+    case CHAR_STATE_ROTATE: UpdateRotate();
+      break;
+    case CHAR_STATE_FALL: break;
+    case CHAR_STATE_ATTACK: break;
+    case CHAR_STATE_HIT: break;
+    case CHAR_STATE_DIE: break;
+    case CHAR_STATE_MAX: break;
+    default: ;
     }
 
     UpdateGrounded();
