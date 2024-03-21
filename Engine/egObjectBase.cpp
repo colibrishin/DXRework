07525b02826cca7a9fee9a912e2f07aed0e98073
Colibri Shin <colibrishin@gmail.com>
2024-03-21 18:38:23 +0900

    feat/objectBase: remove script in imgui

--- Engine/egObjectBase.cpp
+++ Engine/egObjectBase.cpp
@@ -243,6 +243,14 @@ namespace Engine::Abstract
     return {};
   }
 
+  void ObjectBase::removeScript(const eScriptType type)
+  {
+    if (m_scripts_.contains(type))
+    {
+      m_scripts_.erase(type);
+    }
+  }
+
   WeakComponent ObjectBase::addComponent(const StrongComponent& component)
   {
     const auto type = component->GetComponentType();
@@ -597,6 +605,14 @@ namespace Engine::Abstract
       {
         for (const auto& script : m_scripts_ | std::views::values)
         {
+          std::string script_id = "Remove###" + std::to_string(script->GetID());
+          if (ImGui::Button(script_id.c_str()))
+          {
+            removeScript(script->GetScriptType());
+          }
+
+          ImGui::SameLine();
+
           if (ImGui::TreeNode(script->GetTypeName().c_str()))
           {
             script->OnImGui();
@@ -613,6 +629,14 @@ namespace Engine::Abstract
       {
         for (const auto& comp : m_components_ | std::views::values)
         {
+          std::string comp_id = "Remove###" + std::to_string(comp->GetID());
+          if (ImGui::Button(comp_id.c_str()))
+          {
+            removeComponentImpl(comp->GetID());
+          }
+
+          ImGui::SameLine();
+
           if (ImGui::TreeNode(comp->GetTypeName().c_str()))
           {
             comp->OnImGui();
