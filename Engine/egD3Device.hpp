e1d0d71967376ac4849a229eb73e211e29ed1b93
Colibri Shin <colibrishin@gmail.com>
2024-05-21 20:41:33 +0900

    feat: rewrite d3device with dx12

--- Engine/egD3Device.hpp
+++ Engine/egD3Device.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <d2d1.h>
 #include <d3d12.h>
+#include <d3dcompiler.h>
+#include <dxgi1_4.h>
 #include <dxgi1_5.h>
 #include <dxgidebug.h>
 
@@ -16,6 +18,16 @@ namespace Engine::Manager::Graphics
   using namespace DirectX;
   using Microsoft::WRL::ComPtr;
   using namespace Engine::Graphics;
+
+  const std::map<GUID, eShaderType, GUIDComparer> g_shader_enum_type_map =
+    {
+    {__uuidof(ID3D11VertexShader), SHADER_VERTEX},
+    {__uuidof(ID3D11PixelShader), SHADER_PIXEL},
+    {__uuidof(ID3D11GeometryShader), SHADER_GEOMETRY},
+    {__uuidof(ID3D11ComputeShader), SHADER_COMPUTE},
+    {__uuidof(ID3D11HullShader), SHADER_HULL},
+    {__uuidof(ID3D11DomainShader), SHADER_DOMAIN}
+  };
 
   const std::unordered_map<std::wstring, eShaderType> g_shader_type_map = {
     {L"vs", SHADER_VERTEX}, {L"ps", SHADER_PIXEL}, {L"gs", SHADER_GEOMETRY},
@@ -51,11 +63,16 @@ namespace Engine::Manager::Graphics
     }
 
     static float GetAspectRatio();
+    void         UpdateRenderTarget() const;
+
+    void CreateShaderResourceView(
+      ID3D12Resource * resource, const D3D12_SHADER_RESOURCE_VIEW_DESC & srv, ID3D11ShaderResourceView ** view
+    ) const;
 
     std::vector<std::pair<D3D12_INPUT_ELEMENT_DESC, std::string>> GenerateInputDescription(
       ID3DBlob* blob
     );
-    void CreateSampler(const D3D12_SAMPLER_DESC& description, const D3D12_CPU_DESCRIPTOR_HANDLE& sampler_handle) const;
+    void CreateSampler(const D3D12_SAMPLER_DESC& description, const D3D12_CPU_DESCRIPTOR_HANDLE& handle) const;
 
     void CreateConstantBufferView(
       const UINT slot, const D3D12_CONSTANT_BUFFER_VIEW_DESC & description
@@ -73,31 +90,13 @@ namespace Engine::Manager::Graphics
     const Matrix& GetOrthogonalMatrix() const { return m_ortho_matrix_; }
 
     ID3D12Device* GetDevice() const { return m_device_.Get(); }
+    
+    void __fastcall CopySwapchain(ID3D12Resource* buffer, ID3D12GraphicsCommandList* command_list) const;
 
-    [[nodiscard]] HANDLE                      GetSwapchainAwaiter() const;
-    [[nodiscard]] ID3D12GraphicsCommandList1* GetDirectCommandList(UINT frame_idx = -1) const;
-    [[nodiscard]] ID3D12GraphicsCommandList1* GetCopyCommandList(UINT frame_idx = -1) const;
-    [[nodiscard]] ID3D12GraphicsCommandList1* GetComputeCommandList(UINT frame_idx = -1)const;
-    [[nodiscard]] ID3D12GraphicsCommandList1* GetSubDirectCommandList(UINT frame_idx = -1)const;
-
-    [[nodiscard]] ID3D12CommandQueue* GetDirectCommandQueue() const;
-    [[nodiscard]] ID3D12CommandQueue* GetCopyCommandQueue() const;
-    [[nodiscard]] ID3D12CommandQueue* GetComputeCommandQueue() const;
-    [[nodiscard]] ID3D12CommandQueue* GetSubDirectCommandQueue() const;
-
-    [[nodiscard]] UINT64 GetFrameIndex() const { return m_frame_idx_; }
-
-    void WaitAndReset(const eCommandListIndex type, UINT64 buffer_idx = -1) const;
-    void Wait(const eCommandListIndex type, UINT64 buffer_idx = -1);
-
-    void ExecuteCopyCommandList();
-    void ExecuteComputeCommandList();
-    void ExecuteSubDirectCommandList();
-
-    void CreateTextureFromFile(
-        const std::filesystem::path& file_path, 
-        ID3D12Resource** res, 
-        bool generate_mip) const;
+    [[nodiscard]] HANDLE                            GetSwapchainAwaiter() const;
+    [[nodiscard]] CD3DX12_CPU_DESCRIPTOR_HANDLE     GetRTVHandle() const;
+    [[nodiscard]] CD3DX12_CPU_DESCRIPTOR_HANDLE     GetDSVHandle() const;
+    [[nodiscard]] D3D12_FEATURE_DATA_ROOT_SIGNATURE GetRootSignatureFeature() const;
     
   private:
     friend struct SingletonDeleter;
@@ -117,31 +116,26 @@ namespace Engine::Manager::Graphics
 
     D3Device() = default;
 
+    static void UpdateBuffer(UINT64 size, const void* src, ID3D12Resource* dst);
+
     void InitializeDevice();
+    void InitializeRenderTargetView();
     void InitializeCommandAllocator();
     void InitializeFence();
+    void InitializeD2D();
+    void InitializeDepthStencil();
 
-    void ExecuteDirectCommandList();
-
-    void WaitForEventCompletion(UINT64 buffer_idx) const;
-    void WaitForBackBuffer() const;
+    void WaitForSingleCompletion();
     
-    void CleanupCommandList();
-    void WaitNextFrame();
-
-  private:
-    [[nodiscard]] ID3D12CommandAllocator* GetDirectCommandAllocator(UINT frame_idx = -1) const;
-    [[nodiscard]] ID3D12CommandAllocator* GetCopyCommandAllocator(UINT frame_idx = -1) const;
-    [[nodiscard]] ID3D12CommandAllocator* GetComputeCommandAllocator(UINT frame_idx = -1) const;
-    [[nodiscard]] ID3D12CommandAllocator* GetSubDirectCommandAllocator(UINT frame_idx = -1) const;
-
-    void Reset(const eCommandListIndex type, const UINT64 buffer_idx = -1) const;
-    void Signal(const eCommandListIndex type, UINT64 buffer_idx = -1);
-    UINT64 GetFenceValue(UINT64 buffer_idx = -1) const;
+    void WaitForPreviousFrame();
+    void FrameBegin();
+    void Present() const;
 
     HWND m_hwnd_ = nullptr;
 
-    ComPtr<ID3D12Device2> m_device_ = nullptr;
+    ComPtr<ID3D12Device>        m_device_  = nullptr;
+    ComPtr<IDXGISurface1>     m_surface_                = nullptr;
+    ComPtr<ID2D1RenderTarget> m_d2d_render_target_view_ = nullptr;
 
     UINT s_video_card_memory_        = 0;
     UINT s_refresh_rate_numerator_   = 0;
@@ -149,11 +143,26 @@ namespace Engine::Manager::Graphics
 
     DXGI_ADAPTER_DESC s_video_card_desc_ = {};
 
-    ComPtr<IDXGISwapChain4> m_swap_chain_ = nullptr;
+    ComPtr<IDXGISwapChain4>      m_swap_chain_      = nullptr;
+    ComPtr<ID3D12CommandQueue>   m_command_queue_   = nullptr;
+    
+    ComPtr<ID3D12DescriptorHeap> m_rtv_descriptor_heap_ = nullptr;
+    ComPtr<ID3D12DescriptorHeap> m_dsv_descriptor_heap_ = nullptr;
+    ComPtr<ID3D12DescriptorHeap> m_buffer_descriptor_heap_ = nullptr;
 
-    ComPtr<ID3D12Fence>              m_fence_       = nullptr;
-    HANDLE                           m_fence_event_ = nullptr;
-    std::atomic<UINT64>*             m_fence_nonce_;
+    std::vector<ComPtr<ID3D12Fence>> m_fences_       = {nullptr,};
+    HANDLE                           m_fence_event_  = nullptr;
+    std::vector<UINT64>              m_fences_nonce_ = {0,};
+
+    UINT64                                      m_frame_idx_         = 0;
+    UINT64                                      m_rtv_desc_size_     = 0;
+    std::vector<ComPtr<ID3D12Resource>>         m_render_targets_    = {nullptr,};
+    std::vector<ComPtr<ID3D12CommandAllocator>> m_command_allocator_ = {nullptr,};
+
+    // todo: multi pipeline?
+    ComPtr<ID3D12GraphicsCommandList> m_command_list_ = {nullptr, };
+    
+    D3D11_VIEWPORT s_viewport_{};
 
     UINT64 m_frame_idx_ = 0;
 
