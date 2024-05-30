daf834c04364c040b4037b2ff7e838c2a7c0c15a
Colibri Shin <colibrishin@gmail.com>
2024-05-31 01:00:28 +0900

    fix/sb: write srv/uav to descriptor heap

--- Engine/egStructuredBuffer.hpp
+++ Engine/egStructuredBuffer.hpp
@@ -28,113 +28,7 @@ namespace Engine::Graphics
     void            Create(UINT size, const T* initial_data, bool is_mutable = false);
     void __fastcall SetData(UINT size, const T* src_ptr);
     void __fastcall GetData(UINT size, T* dst_ptr);
-    void            BindSRV();
-    void            UnbindSRV();
     void            Clear();
-
-    void BindSRVGraphicDeferred();
-    void UnbindSRVGraphicDeferred();
-    void BindSRVComputeDeferred();
-    void UnbindSRVComputeDeferred();
-
-    template <typename U = T, typename std::enable_if_t<is_uav_sb<U>::value, bool> = true>
-    void BindUAVGraphicDeferred()
-    {
-      if (m_b_srv_bound_ || m_b_srv_bound_compute_)
-      {
-        throw std::logic_error("StructuredBuffer is bound as SRV, cannot bind as UAV");
-      }
-
-      if (!m_b_uav_bound_ || m_b_uav_bound_compute_) { return; }
-
-      const auto& barrier = CD3DX12_RESOURCE_BARRIER::Transition
-              (
-               m_buffer_.Get(),
-               D3D12_RESOURCE_STATE_COMMON,
-               D3D12_RESOURCE_STATE_UNORDERED_ACCESS
-              );
-    
-      if constexpr (is_client_uav_sb<T>::value == true)
-      {
-        GetD3Device().GetComputeCommandList()->SetComputeRootUnorderedAccessView
-          (
-           which_client_sb_uav<T>::value,
-           m_uav_description_heap_->GetCPUDescriptorHandleForHeapStart()
-          );
-
-      }
-      if constexpr (!is_client_uav_sb<T>::value && is_uav_sb<T>::value)
-      {
-        GetD3Device().GetComputeCommandList()->SetComputeRootUnorderedAccessView
-          (
-           which_sb_uav<T>::value,
-           m_uav_description_heap_->GetCPUDescriptorHandleForHeapStart()
-          );
-      }
-
-      GetD3Device().GetComputeCommandList()->ResourceBarrier(1, &barrier);
-
-      GetD3Device().ExecuteComputeCommandList();
-      
-      m_b_uav_bound_ = true;
-    }
-
-    template <typename U = T, typename std::enable_if_t<is_uav_sb<U>::value, bool> = true>
-    void BindUAVComputeDeferred()
-    {
-      if (m_b_srv_bound_ || m_b_srv_bound_compute_)
-      {
-        throw std::logic_error("StructuredBuffer is bound as SRV, cannot bind as UAV");
-      }
-
-      const auto& barrier = CD3DX12_RESOURCE_BARRIER::Transition
-      (
-       m_buffer_.Get(),
-       D3D12_RESOURCE_STATE_UNORDERED_ACCESS,
-       D3D12_RESOURCE_STATE_COMMON
-      );
-
-      if constexpr (is_client_uav_sb<T>::value == true)
-      {
-        GetD3Device().GetCommandList()->SetComputeRootUnorderedAccessView
-          (
-           which_client_sb_uav<T>::value,
-            0
-          );
-
-      }
-      if constexpr (!is_client_uav_sb<T>::value && is_uav_sb<T>::value)
-      {
-        GetD3Device().GetCommandList()->SetComputeRootUnorderedAccessView
-          (
-           which_sb_uav<T>::value,
-           0
-          );
-      }
-
-      GetD3Device().GetCommandList()->ResourceBarrier(1, &barrier);
-
-      GetD3Device().ExecuteComputeCommandList();
-
-      m_b_uav_bound_ = false;
-    }
-
-    template <typename U = T, typename std::enable_if_t<is_uav_sb<U>::value, bool> = true>
-    void UnbindUAVComputeDeferred()
-    {
-      if (!m_b_uav_bound_compute_) { return; }
-
-      const auto& uav_transition = CD3DX12_RESOURCE_BARRIER::Transition
-        (
-         m_buffer_.Get(),
-         D3D12_RESOURCE_STATE_UNORDERED_ACCESS,
-         D3D12_RESOURCE_STATE_COMMON
-        );
-
-      GetD3Device().GetComputeCommandList()->ResourceBarrier(1, &uav_transition);
-
-      m_b_uav_bound_compute_ = false;
-    }
 
   private:
     void InitializeSRV(UINT size);
@@ -153,10 +47,6 @@ namespace Engine::Graphics
     UINT        m_size_;
 
     ComPtr<ID3D12Resource> m_buffer_;
-    
-    ComPtr<ID3D12DescriptorHeap> m_srv_description_heap_;
-    ComPtr<ID3D12DescriptorHeap> m_uav_description_heap_;
-
     ComPtr<ID3D12Resource> m_write_buffer_;
     ComPtr<ID3D12Resource> m_read_buffer_;
   };
@@ -164,23 +54,6 @@ namespace Engine::Graphics
   template <typename T>
   void StructuredBuffer<T>::InitializeSRV(const UINT size)
   {
-    constexpr D3D12_DESCRIPTOR_HEAP_DESC heap_desc
-    {
-      .Type           = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
-      .NumDescriptors = 1,
-      .Flags          = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
-      .NodeMask       = 0
-    };
-
-    DX::ThrowIfFailed
-      (
-       GetD3Device().GetDevice()->CreateDescriptorHeap
-       (
-        &heap_desc,
-        IID_PPV_ARGS(m_srv_description_heap_.ReleaseAndGetAddressOf())
-       )
-      );
-    
     const D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc
     {
       .Format = DXGI_FORMAT_UNKNOWN,
@@ -195,34 +68,21 @@ namespace Engine::Graphics
       }
     };
 
+    const auto& srv_handle = CD3DX12_CPU_DESCRIPTOR_HANDLE(GetRenderPipeline().GetSRVHeap()->GetCPUDescriptorHandleForHeapStart());
+
+    srv_handle.Offset(which_sb<T>::value, GetRenderPipeline().GetBufferDescriptorSize());
+
     GetD3Device().GetDevice()->CreateShaderResourceView
      (
        m_buffer_.Get(),
        &srv_desc,
-       m_srv_description_heap_->GetCPUDescriptorHandleForHeapStart()
+       srv_handle
      );
   }
 
   template <typename T>
   void StructuredBuffer<T>::InitializeUAV(UINT size)
   {
-    constexpr D3D12_DESCRIPTOR_HEAP_DESC heap_desc
-    {
-      .Type           = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
-      .NumDescriptors = 1,
-      .Flags          = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
-      .NodeMask       = 0
-    };
-    
-    DX::ThrowIfFailed
-      (
-       GetD3Device().GetDevice()->CreateDescriptorHeap
-       (
-        &heap_desc,
-        IID_PPV_ARGS(m_uav_description_heap_.ReleaseAndGetAddressOf())
-       )
-      );
-    
     D3D12_UNORDERED_ACCESS_VIEW_DESC uav_desc;
     uav_desc.Format              = DXGI_FORMAT_UNKNOWN;
     uav_desc.ViewDimension       = D3D12_UAV_DIMENSION_BUFFER;
@@ -232,13 +92,24 @@ namespace Engine::Graphics
     uav_desc.Buffer.CounterOffsetInBytes = 0;
     uav_desc.Buffer.Flags = D3D12_BUFFER_UAV_FLAG_NONE;
 
+    const auto& uav_handle = CD3DX12_CPU_DESCRIPTOR_HANDLE(GetRenderPipeline().GetUAVHeap()->GetCPUDescriptorHandleForHeapStart());
+
+    if constexpr (is_client_sb<T>::value == true)
+    {
+      uav_handle.Offset(which_client_sb<T>::value, GetRenderPipeline().GetBufferDescriptorSize());
+    }
+    else
+    {
+      uav_handle.Offset(which_sb<T>::value, GetRenderPipeline().GetBufferDescriptorSize());
+    }
+
     GetD3Device().GetDevice()->CreateUnorderedAccessView
-     (
+      (
        m_buffer_.Get(),
        nullptr,
        &uav_desc,
-       m_uav_description_heap_->GetCPUDescriptorHandleForHeapStart()
-     );
+       uav_handle
+      );
   }
 
   template <typename T>
@@ -563,69 +434,6 @@ namespace Engine::Graphics
     std::memcpy(dst_ptr, data, sizeof(T) * size);
 
     m_read_buffer_->Unmap(0, nullptr);
-  }
-
-  template <typename T>
-  void StructuredBuffer<T>::BindSRV()
-  {
-    if (m_b_uav_bound_) { throw std::logic_error("StructuredBuffer is already bound as UAV"); }
-
-    {
-      DirectCommandGuard cg;
-      
-      const auto& barrier = CD3DX12_RESOURCE_BARRIER::Transition
-      (
-       m_buffer_.Get(),
-       D3D12_RESOURCE_STATE_COMMON,
-       D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE
-      );
-
-      GetD3Device().GetCommandList()->ResourceBarrier(1, &barrier);
-
-      if constexpr (is_client_sb<T>::value == true)
-      {
-        GetD3Device().GetCommandList()->SetGraphicsRootShaderResourceView
-          (which_client_sb<T>::value, m_buffer_->GetGPUVirtualAddress());
-      }
-      else
-      {
-        GetD3Device().GetCommandList()->SetGraphicsRootShaderResourceView
-          (which_sb<T>::value, m_buffer_->GetGPUVirtualAddress());
-      }
-    }
-    
-    m_b_srv_bound_ = true;
-  }
-
-  // This will execute the command list and copy the data from the GPU to the CPU.
-  template <typename T>
-  void StructuredBuffer<T>::UnbindSRV()
-  {
-    {
-      DirectCommandGuard cg;
-
-      const auto& barrier = CD3DX12_RESOURCE_BARRIER::Transition
-      (
-       m_buffer_.Get(),
-       D3D12_RESOURCE_STATE_NON_PIXEL_SHADER_RESOURCE,
-       D3D12_RESOURCE_STATE_COMMON
-      );
-
-      GetD3Device().GetCommandList()->ResourceBarrier(1, &barrier);
-    
-      if constexpr (is_client_sb<T>::value == true)
-      {
-        GetD3Device().GetCommandList()->SetGraphicsRootShaderResourceView(
-          which_client_sb<T>::value, 0);
-      }
-      else
-      {
-        GetD3Device().GetCommandList()->SetGraphicsRootShaderResourceView(
-          which_sb<T>::value, 0);
-      }
-    }
-    
-    m_b_srv_bound_ = false;
   }
 
   template <typename T>
