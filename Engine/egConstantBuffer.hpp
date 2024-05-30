5f01a9e4511f383a4012eaeef055a75c4961f9e3
Colibri Shin <colibrishin@gmail.com>
2024-05-30 15:26:28 +0900

    feat: add constant buffer

--- Engine/egConstantBuffer.hpp
+++ Engine/egConstantBuffer.hpp
@@ -1,12 +1,7 @@
 #pragma once
-#include <d3dx12.h>
-
-#include "egGlobal.h"
-#include "egRenderPipeline.h"
 
 namespace Engine::Graphics
 {
-  // Creates a constant buffer for only current back buffer.
   template <typename T>
   class ConstantBuffer
   {
@@ -15,10 +10,10 @@ namespace Engine::Graphics
 
     void Create(const T* src_data)
     {
-      GetD3Device().WaitAndReset(COMMAND_IDX_COPY);
+      DirectCommandGuard dcg;
 
       const auto& default_heap = CD3DX12_HEAP_PROPERTIES(D3D12_HEAP_TYPE_DEFAULT);
-      const auto& cb_desc      = CD3DX12_RESOURCE_DESC::Buffer(m_alignment_size_);
+      const auto& cb_desc = CD3DX12_RESOURCE_DESC::Buffer(sizeof(T));
 
       DX::ThrowIfFailed
         (
@@ -48,42 +43,30 @@ namespace Engine::Graphics
            )
           );
 
+        s_upload_buffers_.push_back(upload_buffer);
+
         char* data = nullptr;
 
         DX::ThrowIfFailed(upload_buffer->Map(0, nullptr, reinterpret_cast<void**>(&data)));
         std::memcpy(data, src_data, sizeof(T));
         upload_buffer->Unmap(0, nullptr);
 
-        GetD3Device().GetCopyCommandList()->CopyResource(m_buffer_.Get(), upload_buffer.Get());
+        GetD3Device().GetCommandList()->CopyResource(m_buffer_.Get(), upload_buffer.Get());
       }
 
-      const auto& cb_trans = CD3DX12_RESOURCE_BARRIER::Transition
-        (m_buffer_.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
+      const auto& cb_trans = CD3DX12_RESOURCE_BARRIER::Transition(m_buffer_.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
 
-      GetD3Device().GetCopyCommandList()->ResourceBarrier(1, &cb_trans);
-
-      const D3D12_CONSTANT_BUFFER_VIEW_DESC cbv_desc
-      {
-        .BufferLocation = m_buffer_->GetGPUVirtualAddress(),
-        .SizeInBytes    = m_alignment_size_
-      };
-
-      DX::ThrowIfFailed(GetD3Device().GetCopyCommandList()->Close());
-
-      GetD3Device().ExecuteCopyCommandList();
-
-      GetD3Device().CreateConstantBufferView(which_cb<T>::value, cbv_desc);
+      GetD3Device().GetCommandList()->ResourceBarrier(1, &cb_trans);
     }
 
     void SetData(const T* src_data)
     {
-      GetD3Device().WaitAndReset(COMMAND_IDX_COPY);
+      DirectCommandGuard dcg;
 
       const auto& copy_trans = CD3DX12_RESOURCE_BARRIER::Transition(m_buffer_.Get(), D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, D3D12_RESOURCE_STATE_COPY_DEST);
 
-      GetD3Device().GetCopyCommandList()->ResourceBarrier(1, &copy_trans);
+      GetD3Device().GetCommandList()->ResourceBarrier(1, &copy_trans);
 
-      // Use upload buffer for synchronization.
       ComPtr<ID3D12Resource> upload_buffer;
 
       DX::ThrowIfFailed
@@ -97,6 +80,8 @@ namespace Engine::Graphics
          )
         );
 
+      s_upload_buffers_.push_back(upload_buffer);
+
       char* data = nullptr;
 
       DX::ThrowIfFailed(upload_buffer->Map(0, nullptr, reinterpret_cast<void**
@@ -106,29 +91,69 @@ namespace Engine::Graphics
 
       upload_buffer->Unmap(0, nullptr);
 
-      GetD3Device().GetCopyCommandList()->CopyResource(m_buffer_.Get(), upload_buffer.Get());
+      GetD3Device().GetCommandList()->CopyResource(m_buffer_.Get(), upload_buffer.Get());
 
       const auto& cb_trans = CD3DX12_RESOURCE_BARRIER::Transition(m_buffer_.Get(), D3D12_RESOURCE_STATE_COPY_DEST, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER);
 
-      GetD3Device().GetCopyCommandList()->ResourceBarrier(1, &cb_trans);
+      GetD3Device().GetCommandList()->ResourceBarrier(1, &cb_trans);
 
-      DX::ThrowIfFailed(GetD3Device().GetCopyCommandList()->Close());
+      constexpr D3D12_DESCRIPTOR_HEAP_DESC heap_desc
+      {
+        .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+        .NumDescriptors = 1,
+        .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+        .NodeMask = 0
+      };
 
-      GetD3Device().ExecuteCopyCommandList();
+      DX::ThrowIfFailed
+        (GetD3Device().GetDevice()->CreateDescriptorHeap(&heap_desc, IID_PPV_ARGS(m_cbv_heap_.GetAddressOf())));
+
+      m_cbv_desc_.BufferLocation = m_buffer_->GetGPUVirtualAddress();
+      m_cbv_desc_.SizeInBytes = sizeof(T);
+
+      GetD3Device().GetDevice()->CreateConstantBufferView(
+          &m_cbv_desc_, 
+          m_cbv_heap_->GetCPUDescriptorHandleForHeapStart());
+    }
+
+    void Bind() const
+    {
+      DirectCommandGuard dcg;
+
+      GetD3Device().GetCommandList()->SetGraphicsRootConstantBufferView(which_cb<T>::value, m_buffer_->GetGPUVirtualAddress());
+    }
+
+    void Unbind() const
+    {
+      DirectCommandGuard dcg;
+
+      GetD3Device().GetCommandList()->SetGraphicsRootConstantBufferView(which_cb<T>::value, 0);
+    }
+
+    static void Reset()
+    {
+      for (auto& buffer : s_upload_buffers_)
+      {
+        buffer.Reset();
+      }
+
+      s_upload_buffers_.clear();
     }
 
   private:
+    inline static std::vector<ComPtr<ID3D12Resource>> s_upload_buffers_ = {};
 
+    ComPtr<ID3D12DescriptorHeap> m_cbv_heap_;
     ComPtr<ID3D12Resource> m_buffer_;
-    UINT m_alignment_size_;
+    D3D12_CONSTANT_BUFFER_VIEW_DESC m_cbv_desc_;
 
   };
 
   template <typename T>
   ConstantBuffer<T>::ConstantBuffer()
+    : m_cbv_desc_()
   {
-    static_assert(std::is_standard_layout_v<T>, "Constant buffer type must be a POD type");
-
-    m_alignment_size_ = (sizeof(T) + 255) & ~255; 
+    static_assert(sizeof(T) % 256 == 0, "Constant buffer size must be 256-byte aligned");
+    static_assert(std::is_pod_v<T>, "Constant buffer type must be a POD type");
   }
 }
