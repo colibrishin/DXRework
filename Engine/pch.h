de0e5f8b8c3e688de7261e991bb8cf3cf1a084c8
Colibri Shin <colibrishin@gmail.com>
2024-05-31 15:55:14 +0900

    proj: add dx12 helper package

--- Engine/pch.h
+++ Engine/pch.h
@@ -86,8 +86,8 @@
 
 #include <d2d1.h>
 #include <d3d12.h>
-#include "../DX-Headers/include/directx/d3dx12.h"
 #include <d3dcompiler.h>
+#include <d3dx12.h>
 #include <directxmath.h>
 #include <dxcapi.h>
 #include <dxgi1_5.h>
