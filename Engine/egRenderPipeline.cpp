fee05524e8a28de00551afcc3b11f1be5e946095
Colibri Shin <colibrishin@gmail.com>
2024-05-31 21:16:01 +0900

    fix/tex: missing state transition
    feat/tex: split dsv and dsv only case

--- Engine/egRenderPipeline.cpp
+++ Engine/egRenderPipeline.cpp
@@ -26,6 +26,8 @@ namespace Engine::Manager::Graphics
 
   void RenderPipeline::DefaultRenderTarget() const
   {
+    DirectCommandGuard dcg;
+
     const auto& rtv_handle = m_rtv_descriptor_heap_->GetCPUDescriptorHandleForHeapStart();
     const auto& dsv_handle = m_dsv_descriptor_heap_->GetCPUDescriptorHandleForHeapStart();
 
@@ -680,6 +682,24 @@ namespace Engine::Manager::Graphics
   {
     DirectCommandGuard dcg;
     GetD3Device().GetCommandList()->OMSetRenderTargets(1, &rtv_dsv_pair.first, false, &rtv_dsv_pair.second);
+  }
+
+  RTVDSVHandlePair RenderPipeline::SetDepthStencilDeferred(const D3D12_CPU_DESCRIPTOR_HANDLE& dsv) const
+  {
+    DirectCommandGuard dcg;
+
+    const auto& current_rtv = CD3DX12_CPU_DESCRIPTOR_HANDLE
+      (
+       m_rtv_descriptor_heap_->GetCPUDescriptorHandleForHeapStart(),
+       GetD3Device().GetFrameIndex(),
+       m_rtv_descriptor_size_
+      );
+
+    const auto& current_dsv = m_dsv_descriptor_heap_->GetCPUDescriptorHandleForHeapStart();
+
+    GetD3Device().GetCommandList()->OMSetRenderTargets(1, &current_rtv, false, &dsv);
+
+    return {current_rtv, current_dsv};
   }
 
   RTVDSVHandlePair RenderPipeline::SetDepthStencilOnlyDeferred(const D3D12_CPU_DESCRIPTOR_HANDLE& dsv) const
