b7731aa5033fbaf66d7adf52edb08f9e7643a5a0
Colibri Shin <colibrishin@gmail.com>
2024-06-03 21:27:53 +0900

    fix: remove d2d

--- Engine/pch.h
+++ Engine/pch.h
@@ -80,11 +80,9 @@
 
 #pragma comment(lib, "d3d12.lib")
 #pragma comment(lib, "dxguid.lib")
-#pragma comment(lib, "d2d1.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "d3dcompiler.lib")
 
-#include <d2d1.h>
 #include <d3d12.h>
 #include <d3dcompiler.h>
 #include <d3dx12.h>
