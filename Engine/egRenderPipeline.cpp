ee204d6ce47c985f2f286bd7ef7f53250f5d29c9
Colibri Shin <colibrishin@gmail.com>
2024-06-03 21:40:58 +0900

    fix/RP: wrong format type for RTV

--- Engine/egRenderPipeline.cpp
+++ Engine/egRenderPipeline.cpp
@@ -295,7 +295,7 @@ namespace Engine::Manager::Graphics
 
     constexpr D3D12_RENDER_TARGET_VIEW_DESC rtv_desc
     {
-      .Format = DXGI_FORMAT_B8G8R8A8_UNORM,
+      .Format = DXGI_FORMAT_R8G8B8A8_UNORM,
       .ViewDimension = D3D12_RTV_DIMENSION_TEXTURE2D,
       .Texture2D = { 0, 0 }
     };
