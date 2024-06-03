9ac4cedc7bc8985ddd95e6dc1aed8beed3440c9a
Colibri Shin <colibrishin@gmail.com>
2024-06-03 21:40:41 +0900

    fix/RP: rename cb upload func

--- Engine/egRenderPipeline.h
+++ Engine/egRenderPipeline.h
@@ -92,7 +92,7 @@ namespace Engine::Manager::Graphics
     UINT GetBufferDescriptorSize() const;
     UINT GetSamplerDescriptorSize() const;
 
-    void UploadConstantBuffersDeferred();
+    void UploadConstantBuffers();
     void ExecuteDirectCommandList();
 
   private:
