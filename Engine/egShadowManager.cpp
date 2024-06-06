9f5429a5333c9847593a82e5a5d8f25336b8d5d1
Colibri Shin <colibrishin@gmail.com>
2024-06-07 01:12:54 +0900

    fix/CB: update data before binding

--- Engine/egShadowManager.cpp
+++ Engine/egShadowManager.cpp
@@ -193,7 +193,7 @@ namespace Engine::Manager::Graphics
          const auto& dsv = m_shadow_texs_.at(light->GetLocalID());
          m_shadow_map_mask_.Bind(cmd, dsv);
 
-         GetRenderPipeline().UploadConstantBuffers(heap);
+         GetRenderPipeline().BindConstantBuffers(heap);
 
          heap->SetSampler(m_sampler_heap_->GetCPUDescriptorHandleForHeapStart(), SAMPLER_SHADOW);
 
