7e989bf1585014f39b7cbe768f1d63dfb3af3a49
Colibri Shin <colibrishin@gmail.com>
2024-06-09 23:13:16 +0900

    fix/toolkit/debug: following command changes

--- Engine/egToolkitAPI.cpp
+++ Engine/egToolkitAPI.cpp
@@ -83,20 +83,20 @@ namespace Engine::Manager::Graphics
 
     ID3D12DescriptorHeap* heaps[] = { m_descriptor_heap_->Heap(), m_states_->Heap() };
 
-    const auto& s_cmd = GetD3Device().AcquireCommandPair(L"Toolkit Sprite Render");
+    const auto& s_cmd = GetD3Device().AcquireCommandPair(L"Toolkit Sprite Render").lock();
 
-    s_cmd.SoftReset();
+    s_cmd->SoftReset();
 
     m_sprite_batch_->Begin
     (
-        s_cmd.GetList(), 
+        s_cmd->GetList(), 
         SpriteSortMode_Deferred
     );
 
     GetRenderPipeline().DefaultRenderTarget(s_cmd);
     GetRenderPipeline().DefaultScissorRect(s_cmd);
     GetRenderPipeline().DefaultViewport(s_cmd);
-    s_cmd.GetList()->SetDescriptorHeaps(2, heaps);
+    s_cmd->GetList()->SetDescriptorHeaps(2, heaps);
 
     for (const auto& callback : m_sprite_batch_callbacks_)
     {
@@ -105,18 +105,20 @@ namespace Engine::Manager::Graphics
 
     m_sprite_batch_->End();
 
-    const auto& p_cmd = GetD3Device().AcquireCommandPair(L"Toolkit Primitive Render");
+    s_cmd->FlagReady();
 
-    p_cmd.SoftReset();
+    const auto& p_cmd = GetD3Device().AcquireCommandPair(L"Toolkit Primitive Render").lock();
 
-    m_basic_effect_->Apply(p_cmd.GetList());
+    p_cmd->SoftReset();
+
+    m_basic_effect_->Apply(p_cmd->GetList());
 
     GetRenderPipeline().DefaultRenderTarget(p_cmd);
     GetRenderPipeline().DefaultScissorRect(p_cmd);
     GetRenderPipeline().DefaultViewport(p_cmd);
-    p_cmd.GetList()->SetDescriptorHeaps(2, heaps);
+    p_cmd->GetList()->SetDescriptorHeaps(2, heaps);
 
-    m_primitive_batch_->Begin(p_cmd.GetList());
+    m_primitive_batch_->Begin(p_cmd->GetList());
 
     for (const auto& callback : m_primitive_batch_callbacks_)
     {
@@ -128,6 +130,8 @@ namespace Engine::Manager::Graphics
     m_sprite_batch_callbacks_.clear();
 
     m_primitive_batch_callbacks_.clear();
+
+    p_cmd->FlagReady();
 
     m_graphics_memory_->Commit(GetD3Device().GetCommandQueue(COMMAND_TYPE_DIRECT));
   }
