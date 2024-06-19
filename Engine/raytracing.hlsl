112011106de978790a2fa6edcdb55f5619cd18b4
Colibri Shin <colibrishin@gmail.com>
2024-06-19 19:08:31 +0900

    bug/feat/shader: TraceRay in raytracing
    fixing where index value dropped by casting into float

--- Engine/raytracing.hlsl
+++ Engine/raytracing.hlsl
@@ -2,9 +2,16 @@
 
 RWTexture2D<float4> g_output : register(u0);
 
+RaytracingAccelerationStructure g_tlas : register(t0);
+
 struct Payload
 {
   float4 colorAndDist;
+};
+
+struct Attributes
+{
+  float2 barycentrics;
 };
 
 [shader("raygeneration")]
@@ -14,5 +21,39 @@ void raygen_main()
   uint2 dispatchRaysIndex = DispatchRaysIndex().xy;
   uint2 dispatchRaysDimensions = DispatchRaysDimensions().xy;
 
-  g_output[dispatchRaysIndex.xy] = float4(0.f, 0.f, 0.f, 1.0f);
+  const float2 mid_idx = dispatchRaysIndex.xy + 0.5f;
+  float2 ndc = mid_idx / dispatchRaysDimensions.xy * 2.f - 1.f;
+  ndc.y = -ndc.y;
+
+  float4 world = mul(float4(ndc, 0, 1), g_camInvVP);
+  world /= world.w;
+
+  float4 origin = mul(float4(0, 0, 0, 1), g_camWorld);
+  origin /= origin.w;
+
+  RayDesc ray;
+  ray.Origin = origin.xyz;
+  ray.Direction = normalize(world.xyz - ray.Origin.xyz);
+  ray.TMin = 0.001f;
+  ray.TMax = 1000.0f;
+
+  Payload payload;
+
+  TraceRay(g_tlas, RAY_FLAG_CULL_BACK_FACING_TRIANGLES, 0xFF, 0, 0, 0, ray, payload);
+
+  g_output[dispatchRaysIndex.xy] = payload.colorAndDist;
+}
+
+
+[shader("closesthit")]
+void closest_hit_main(inout Payload payload, Attributes attr)
+{
+  payload.colorAndDist = float4(1.0f, 0.0f, 0.0f, 1.0f);
+  payload.colorAndDist.w = RayTCurrent();
+}
+
+[shader("miss")]
+void miss_main(inout Payload payload)
+{
+  payload.colorAndDist = float4(g_ambientColor.xyz, 1.0f);
 }
