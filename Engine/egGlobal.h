ae4511d581c184fce51fb29819a7b6f4a17e3b15
Colibri Shin <colibrishin@gmail.com>
2024-06-26 18:28:43 +0900

    feat/global: add pause flag

--- Engine/egGlobal.h
+++ Engine/egGlobal.h
@@ -12,4 +12,10 @@ namespace Engine
   inline std::atomic<float> g_screen_far    = 100.0f;
   inline std::atomic<UINT>  g_frame_buffer  = 2;
   inline std::atomic<bool>  g_raytracing    = false;
+
+  // Debugging Modifier
+  inline std::atomic<bool>  g_paused        = false;
+  inline std::atomic<bool>  g_camera_lock   = false;
+  // Not thread safe!
+  inline float              g_camera_speed  = 0.1f;
 }
