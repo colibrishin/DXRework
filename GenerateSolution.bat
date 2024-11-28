226b1ad2d0c15b67d3c782a28ebaf4d1fb110cec
DESKTOP-COA1C72\AR student <test@gmail.com>
2024-11-28 09:46:07 +0900

    misc: remove quotation marks

--- GenerateSolution.bat
+++ GenerateSolution.bat
@@ -21,7 +21,7 @@ IF NOT "%~4"=="" set SharpMakeSolutionDir="%4"
 echo SharpMakeSolutionDir: %SharpMakeSolutionDir%
 
 set VSVcpkg="%PROGRAMFILES%\Microsoft Visual Studio\2022\Community\VC\vcpkg\vcpkg.exe"
-IF EXIST %VSVcpkg% (echo "Found Visual studo vcpkg") else (echo "No vcpkg from Visual studio found, fallback to env path")
+IF EXIST %VSVcpkg% (echo Found Visual studo vcpkg) else (echo No vcpkg from Visual studio found, fallback to env path)
 IF EXIST %VSVcpkg% (%VSVcpkg% install) ELSE (vcpkg install)
 
 "%EngineDir%\Programs\Sharpmake\Sharpmake.Application\bin\Release\net6.0\Sharpmake.Application.exe" /sources(@'%TargetCS%') /verbose
