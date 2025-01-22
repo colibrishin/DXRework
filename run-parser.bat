214e2b4965418f2606f2e1c260cba6ee32c2ec5a
Colibri Shin <colibrishin@gmail.com>
2025-01-22 18:33:35 +0900

    fix: pid check for header parser

--- run-parser.bat
+++ run-parser.bat
@@ -1,5 +1,5 @@
-title HeaderParserBatchFile
 @echo off
+setlocal enabledelayedexpansion
 rem [EngineDir] [ProjectName] [ProjectDir] [WINDIR] [GITPATH]
 
 if %1=="" (goto EXIT)
@@ -11,6 +11,7 @@ if %5=="" (goto EXIT)
 set "XCOPY_PATH=%4\System32\xcopy.exe"
 set "GITPATH=%5"
 set "SH_PATH=%GITPATH:"=%\bin\sh.exe"
+set "TASKLIST_PATH=%4\System32\tasklist.exe"
 
 echo Given project directory is %2
 echo Given root directory is %1
@@ -23,11 +24,21 @@ mkdir Intermediate\HeaderParser\HeaderGenerated
 )
 
 :WAIT_LOCK
+if exist lock (
+set /p PID=<lock
+echo Waiting %PID%
+%TASKLIST_PATH% /fi "PID eq %PID%" && (
+del lock
+goto CRITICAL) || (goto WAIT_LOCK))
 echo Check other instances...
 if not exist lock (goto CRITICAL) else (goto WAIT_LOCK)
 
 :CRITICAL
-tasklist /V /FI "WindowTitle eq HeaderParserBatchFile*" /FO csv /nh  > lock
+call getCmdPID.bat
+set PID=%errorlevel%
+if not exist lock (
+echo !PID! > lock) else (
+goto WAIT_LOCK)
 
 if not exist Intermediate\HeaderParser\.git (
 echo Header tracking git repository is not initialized... Initializing...
@@ -39,13 +50,25 @@ popd
 )
 
 pushd Intermediate\HeaderParser
+type nul > target
 echo Copying header from %3...
 if not exist %2 (mkdir %2)
-for /r %3 %%f in (*.h) do %XCOPY_PATH% %%f %2 /y /d
+for /r %3 %%f in (*.h) do (
+%XCOPY_PATH% %%f %2 /y /d
+echo HeaderGenerated\%~n3\%%~nf.generated.h
+if not exist HeaderGenerated\%~n3\%%~nf.generated.h (
+echo Header does not generated
+echo %~n3\%%~nxf>> target
+))
 
 echo Comparing header changes...
-set "COMMAND=git status --porcelain -uall | cut -c 1-3 --complement | egrep .h$ > target && git add . && git commit -m header-update"
-start "" /b /wait "%SH_PATH%" -c "%COMMAND%"
+set "COMMAND_OW=git status --porcelain -uall | cut -c 1-3 --complement | egrep .h$ > target && git add . && git commit -m header-update"
+set "COMMAND_AP=git status --porcelain -uall | cut -c 1-3 --complement | egrep .h$ >> target && git add . && git commit -m header-update"
+if exist target (
+echo Appending
+start "" /b /wait "%SH_PATH%" -c "%COMMAND_AP%") else (
+echo Overwritting
+start "" /b /wait "%SH_PATH%" -c "%COMMAND_OW%")
 echo Parsing header of %2...
 for /f "tokens=*" %%a in (target) do (
   start "" /b /wait "..\..\Programs\header-parser\Release\header-parser.exe" %%a -c ECLASS -e EENUM -f EFUNC -p EPROPERTY 2>&1
