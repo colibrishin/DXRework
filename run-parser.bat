08450c397a4c4ba008240e433bb47d65e75a2049
Colibri Shin <colibrishin@gmail.com>
2025-01-22 18:33:33 +0900

    fix: header parser lock adjustment

--- run-parser.bat
+++ run-parser.bat
@@ -1,5 +1,16 @@
+title HeaderParserBatchFile
 @echo off
 rem [EngineDir] [ProjectName] [ProjectDir] [WINDIR] [GITPATH]
+
+if %1=="" (goto EXIT)
+if "%2"=="" (goto EXIT)
+if %3=="" (goto EXIT)
+if %4=="" (goto EXIT)
+if %5=="" (goto EXIT)
+
+set "XCOPY_PATH=%4\System32\xcopy.exe"
+set "GITPATH=%5"
+set "SH_PATH=%GITPATH:"=%\bin\sh.exe"
 
 echo Given project directory is %2
 echo Given root directory is %1
@@ -13,30 +24,36 @@ mkdir Intermediate\HeaderParser\HeaderGenerated
 
 :WAIT_LOCK
 echo Check other instances...
-if exist (lock) do goto WAIT_LOCK
-rem todo: PID를 저장하고 진행중인 배치파일이 살아있는지 한번 더 확인
-type nul > lock
+if not exist lock (goto CRITICAL) else (goto WAIT_LOCK)
+
+:CRITICAL
+tasklist /V /FI "WindowTitle eq HeaderParserBatchFile*" /FO csv /nh  > lock
 
 if not exist Intermediate\HeaderParser\.git (
 echo Header tracking git repository is not initialized... Initializing...
 pushd Intermediate\HeaderParser
 echo target >> .gitignore
 echo HeaderGenerated/*** >> .gitignore
-%5\bin\sh.exe --login -c "git init && git add . && git commit -m "Initialization""
+start "" /b /wait "%SH_PATH%" -c "git init && git add . && git commit -m Init"
 popd
 )
 
 pushd Intermediate\HeaderParser
 echo Copying header from %3...
 if not exist %2 (mkdir %2)
-for /r %3 %%f in (*.h) do %4\System32\xcopy %%f %2 /y /d
+for /r %3 %%f in (*.h) do %XCOPY_PATH% %%f %2 /y /d
 
 echo Comparing header changes...
-%5\bin\sh.exe --login -c "git status --porcelain -uall | cut -c 1-3 --complement | egrep .h$ > target && git add . && git commit -m "header-update""
+set "COMMAND=git status --porcelain -uall | cut -c 1-3 --complement | egrep .h$ > target && git add . && git commit -m header-update"
+start "" /b /wait "%SH_PATH%" -c "%COMMAND%"
 echo Parsing header of %2...
 for /f "tokens=*" %%a in (target) do (
-  "..\..\Programs\header-parser\Release\header-parser.exe" %%a -c ECLASS -e EENUM -f EFUNC -p EPROPERTY
+  start "" /b /wait "..\..\Programs\header-parser\Release\header-parser.exe" %%a -c ECLASS -e EENUM -f EFUNC -p EPROPERTY 2>&1
 )
 popd
-
 del lock
+exit /b
+
+:EXIT
+echo Insufficient argument given.
+exit /b
