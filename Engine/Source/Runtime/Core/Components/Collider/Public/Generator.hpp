f01c0b508fd81e13841b147c93b109e462038cde
Colibri Shin <colibrishin@gmail.com>
2025-01-22 18:33:41 +0900

    fix: cube generator
    fix: degenerate cube vertices
    
    fix: misinterpret of cube vertice generation (+1 squashed commits)
    
    Squashed commits:
    
    [291b81b] fix: degenerate tex coord, evaluate tangent and binormal

--- Engine/Source/Runtime/Core/Components/Collider/Public/Generator.hpp
+++ Engine/Source/Runtime/Core/Components/Collider/Public/Generator.hpp
@@ -1,16 +1,141 @@
 #pragma once
 #define _USE_MATH_DEFINES
 #include <math.h>
-
-#include <gcem.hpp>
 #include <array>
+#include <gcem.hpp>
 
 #include "TypeLibrary/Public/TypeLibrary.h"
 #include "VertexElement/Public/VertexElement.h"
 
 namespace Engine 
 {
-	template <size_t Longitutde = 16, size_t Latitudue = 16>
+	namespace details
+	{
+		constexpr Vector3 Normalize(const Vector3& v)
+		{
+			const float magnitude = gcem::sqrt(gcem::pow(v.x, 2) + gcem::pow(v.y, 2) + gcem::pow(v.z, 2));
+
+			if (magnitude == 0.f)
+			{
+				return Vector3{0.f, 0.f, 0.f};
+			}
+
+			return Vector3{v.x == 0.f ? 0.f : v.x / magnitude, v.y == 0.f ? 0.f : v.y / magnitude, v.z == 0.f ? 0.f : v.z / magnitude};
+		}
+
+		constexpr Vector3 Cross(const Vector3& lhs, const Vector3& rhs)
+		{
+			return Vector3
+			{
+				lhs.y * rhs.z - lhs.z * rhs.y,
+				lhs.z * rhs.x - lhs.x * rhs.z,
+				lhs.x * rhs.y - lhs.y * rhs.x
+			};
+		}
+
+		constexpr Vector3 Subtract(const Vector3& lhs, const Vector3& rhs)
+		{
+			return {lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z};
+		}
+
+		constexpr Vector3 Add(const Vector3& lhs, const Vector3& rhs)
+		{
+			return {lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z};
+		}
+
+		constexpr Vector3 Multiply(const Vector3& lhs, const float s)
+		{
+			return {s * lhs.x, s * lhs.y, s * lhs.z};
+		}
+		
+		constexpr void GenerateTangentBinormal(
+				const Vector3& v0, const Vector3&  v1,
+				const Vector3& v2, const Vector2&  uv0,
+				const Vector2& uv1, const Vector2& uv2,
+				Vector3&       tangent, Vector3&   binormal
+			)
+		{
+			const Vector3 edge1 = {v1.x - v0.x, v1.y - v0.y, v1.z - v0.z};
+			const Vector3 edge2 = {v2.x - v0.x, v2.y - v0.y, v2.z - v0.z};
+
+			const Vector2 deltaUV1 = {uv1.x - uv0.x, uv2.x - uv0.x};
+			const Vector2 deltaUV2 = {uv1.y - uv0.y, uv2.y - uv0.y};
+
+			if (const float delta = (deltaUV1.x * deltaUV2.y) - (deltaUV1.y * deltaUV2.x);
+				delta != 0.f)
+			{
+				const float denominator = 1.0f / delta;
+
+				tangent.x = denominator * (deltaUV2.y * edge1.x - deltaUV1.y * edge2.x);
+				tangent.y = denominator * (deltaUV2.y * edge1.y - deltaUV1.y * edge2.y);
+				tangent.z = denominator * (deltaUV2.y * edge1.z - deltaUV1.y * edge2.z);
+
+				binormal.x = denominator * (-deltaUV2.x * edge1.x + deltaUV1.x * edge2.x);
+				binormal.y = denominator * (-deltaUV2.x * edge1.y + deltaUV1.x * edge2.y);
+				binormal.z = denominator * (-deltaUV2.x * edge1.z + deltaUV1.x * edge2.z);
+			}
+			else
+			{
+				tangent.x = tangent.y = tangent.z = 0.f;
+				binormal.x = binormal.y = binormal.z = 0.f;
+			}
+		}
+		
+		constexpr void GenerateTangentBinormal(auto& vertices, const auto& indices)
+		{
+			struct FacePair
+			{
+				Graphics::VertexElement* o[3];
+			};
+
+			if (indices.size() % 3 != 0)
+			{
+				return;
+			}
+
+			std::vector<FacePair> faces;
+
+			for (size_t i = 0; i < indices.size(); i += 3)
+			{
+				const auto& i0 = indices[i];
+				const auto& i1 = indices[i + 1];
+				const auto& i2 = indices[i + 2];
+
+				FacePair face{&vertices[i0], &vertices[i1], &vertices[i2]};
+				faces.push_back(face);
+			}
+
+			for (const FacePair& face : faces)
+			{
+				Vector3 tangent(0,0,0);
+				Vector3 binormal(0,0,0);
+
+				GenerateTangentBinormal
+					(
+					 face.o[0]->position, face.o[1]->position,
+					 face.o[2]->position, face.o[0]->texCoord,
+					 face.o[1]->texCoord, face.o[2]->texCoord,
+					 tangent, binormal
+					);
+
+				face.o[0]->tangent = Vector3{face.o[0]->tangent.x + tangent.x, face.o[0]->tangent.y + tangent.y, face.o[0]->tangent.z + tangent.z};
+				face.o[1]->tangent = Vector3{face.o[1]->tangent.x + tangent.x, face.o[1]->tangent.y + tangent.y, face.o[1]->tangent.z + tangent.z};
+				face.o[2]->tangent = Vector3{face.o[2]->tangent.x + tangent.x, face.o[2]->tangent.y + tangent.y, face.o[2]->tangent.z + tangent.z};
+
+				face.o[0]->binormal = Vector3{face.o[0]->binormal.x + binormal.x, face.o[0]->binormal.y + binormal.y, face.o[0]->binormal.z + binormal.z};
+				face.o[1]->binormal = Vector3{face.o[1]->binormal.x + binormal.x, face.o[1]->binormal.y + binormal.y, face.o[1]->binormal.z + binormal.z};
+				face.o[2]->binormal = Vector3{face.o[2]->binormal.x + binormal.x, face.o[2]->binormal.y + binormal.y, face.o[2]->binormal.z + binormal.z};
+			}
+
+			for (auto& vertex : vertices)
+			{
+				vertex.tangent = Normalize(vertex.tangent);
+				vertex.binormal = Normalize(vertex.binormal);
+			}
+		}
+	}
+	
+	template <size_t Longitutde = 16, size_t Latitudue = Longitutde * 2>
 	struct SphereGenerator
 	{
 	public:
@@ -62,7 +187,7 @@ namespace Engine
 
 		static consteval std::array<Graphics::VertexElement, s_vertices_count> GenerateSphereSmooth()
 		{
-			float radius = 1;
+			float radius = 0.5f;
 
 			std::array<Graphics::VertexElement, s_vertices_count> collection;
 			float lengthInv = 1.0f / radius;    // normal
@@ -108,6 +233,7 @@ namespace Engine
 				}
 			}
 
+			details::GenerateTangentBinormal(collection, GetSphereIndices());
 			return collection;
 		}
 
@@ -125,86 +251,120 @@ namespace Engine
 		}
 	};
 
-	typedef SphereGenerator<16, 16> DefaultSphereGenerator;
+	typedef SphereGenerator<> DefaultSphereGenerator;
 
 	struct CubeGenerator
 	{
 	private:
-		static constexpr Vector3 vertices[8] =
-		{
-			{-0.5, -0.5, -0.5},
-			{0.5, -0.5, -0.5},
-			{0.5, 0.5, -0.5},
-			{-0.5, 0.5, -0.5},
-			{-0.5, -0.5, 0.5},
-			{0.5, -0.5, 0.5},
-			{0.5, 0.5, 0.5},
-			{-0.5, 0.5, 0.5}
-		};
+		static constexpr size_t faceCount = 6;
 
-		static constexpr Vector2 texCoords[4] =
+		static constexpr Vector2 texCoords[8] =
 		{
+			{0, 1},
+			{0, 0},
+			{0, 1},
+			{1, 1},
+			{0, 1},
 			{0, 0},
 			{1, 0},
-			{1, 1},
-			{0, 1}
+			{1, 1}
 		};
 
-		static constexpr Vector3 normals[6] =
+		static constexpr Vector3 normals[] =
 		{
-			{0, 0, 1},
-			{1, 0, 0},
 			{0, 0, -1},
-			{-1, 0, 0},
+			{0, 0, 1},
 			{0, 1, 0},
-			{0, -1, 0}
+			{0, -1, 0},
+			{-1, 0, 0},
+			{1, 0, 0}
 		};
-
-		static constexpr std::array<UINT, 36> stock_indices =
-		{
-			0, 1, 3, 3, 1, 2,
-			1, 5, 2, 2, 5, 6,
-			5, 4, 6, 6, 4, 7,
-			4, 0, 7, 7, 0, 3,
-			3, 2, 7, 7, 2, 6,
-			4, 5, 0, 0, 5, 1
-		};
-
-		static constexpr uint32_t texInds[6] = { 0, 1, 3, 3, 1, 2 };
-
+	
 	public:
 		static consteval std::array<UINT, 36> GetCubeIndices()
 		{
-			return stock_indices;
-		}
-
-	private:
-		static consteval std::array<Graphics::VertexElement, 8> GenerateCubeVertices()
-		{
-			std::array<Graphics::VertexElement, 8> collection;
-			constexpr auto cube_indices = GetCubeIndices();
-
-			for (size_t i = 0; i < 8; ++i)
+			std::array<UINT, 36> collection{};
+			
+			size_t index_count = 0;
+			size_t vertex_offset = 0;
+			for (size_t i = 0; i < faceCount; ++i)
 			{
-				collection[i] = Graphics::VertexElement
-				(
-					vertices[cube_indices[i]],
-					{ 0.f, 0.f, 1.f, 1.f },
-					texCoords[texInds[i % 4]],
-					normals[cube_indices[i / 6]],
-					Vector3(0.f, 0.f, 0.f),
-					Vector3(0.f, 0.f, 0.f),
-					Graphics::VertexBoneElement()
-				);
+				collection[index_count++] = vertex_offset + 2;
+				collection[index_count++] = vertex_offset + 1;
+				collection[index_count++] = vertex_offset + 0;
+
+				collection[index_count++] = vertex_offset + 3;
+				collection[index_count++] = vertex_offset + 2;
+				collection[index_count++] = vertex_offset + 0;
+
+				vertex_offset += 4;
 			}
 
 			return collection;
 		}
+		
+	private:
+		static consteval std::array<Graphics::VertexElement, 24> GenerateCubeVertices()
+		{
+			std::array<Graphics::VertexElement, 24> collection;
+
+			for (size_t i = 0; i < faceCount; ++i)
+			{
+				const Vector3 normal = normals[i];
+				const Vector3 basis = i >= 4 ? Vector3{ 0.f, 0.f, 1.f } : Vector3{ 0.f, 1.f, 0.f };
+				const Vector3 side1 = details::Cross(normal, basis);
+				const Vector3 side2 = details::Cross(normal, side1);
+
+				collection[vertex_count++] = Graphics::VertexElement
+				(
+					details::Multiply(details::Subtract(details::Subtract(normal, side1), side2), 0.5),
+					Color(1.f, 0.f, 0.f, 1.f),
+					texCoords[0],
+					normal,
+					Vector3(0, 0, 0),
+					Vector3(0, 0, 0),
+					Graphics::VertexBoneElement()
+				);
+				collection[vertex_count++] = Graphics::VertexElement
+				(
+					details::Multiply(details::Add(details::Subtract(normal, side1), side2), 0.5),
+					Color(1.f, 0.f, 0.f, 1.f),
+					texCoords[1],
+					normal,
+					Vector3(0, 0, 0),
+					Vector3(0, 0, 0),
+					Graphics::VertexBoneElement()
+				);
+				collection[vertex_count++] = Graphics::VertexElement
+				(
+					details::Multiply(details::Add(details::Add(normal, side1), side2), 0.5),
+					Color(1.f, 0.f, 0.f, 1.f),
+					texCoords[2],
+					normal,
+					Vector3(0, 0, 0),
+					Vector3(0, 0, 0),
+					Graphics::VertexBoneElement()
+				);
+				collection[vertex_count++] = Graphics::VertexElement
+				(
+					details::Multiply(details::Subtract(details::Add(normal, side1), side2), 0.5),
+					Color(1.f, 0.f, 0.f, 1.f),
+					texCoords[3],
+					normal,
+					Vector3(0, 0, 0),
+					Vector3(0, 0, 0),
+					Graphics::VertexBoneElement()
+				);
+			}
+			
+			details::GenerateTangentBinormal(collection, GetCubeIndices());
+			return collection;
+		}
 
 	public:
-		static consteval std::array<Graphics::VertexElement, 8> GetCubeVertices()
+		static consteval std::array<Graphics::VertexElement, 24> GetCubeVertices()
 		{
-			constexpr std::array<Graphics::VertexElement, 8> vertices = GenerateCubeVertices();
+			constexpr std::array<Graphics::VertexElement, 24> vertices = GenerateCubeVertices();
 			return vertices;
 		}
 	};
