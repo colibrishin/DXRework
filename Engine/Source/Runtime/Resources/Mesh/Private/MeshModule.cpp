bdac67461b01f98c464bcc7d52bfccef2cbb1f27
Colibri Shin <colibrishin@gmail.com>
2025-01-27 00:08:33 +0900

    refactor: point mesh

--- Engine/Source/Runtime/Resources/Mesh/Private/MeshModule.cpp
+++ Engine/Source/Runtime/Resources/Mesh/Private/MeshModule.cpp
@@ -11,6 +11,16 @@ void Engine::MeshModule::Initialize()
 {
     Resources::Mesh::Create("CubeMesh", CubeGenerator::GetCubeVerticesAsVector(), CubeGenerator::GetCubeIndicesAsVector());
     Resources::Mesh::Create("SphereMesh", DefaultSphereGenerator::GetSphereVerticesAsVector(), DefaultSphereGenerator::GetSphereIndicesAsVector());
+    Resources::Mesh::Create(
+         "PointMesh", VertexCollection{ {
+                 {0.f, 0.f, 0.f},
+                 {1.f, 0.f, 0.f, 1.f},
+                 {0.f, 0.f},
+                 {0.f, 0.f, 0.f},
+                 {0.f, 0.f, 0.f},
+                 {0.f, 0.f, 0.f},
+                 Graphics::VertexBoneElement()} }, IndexCollection{ 0 }
+        );
 }
 
 void Engine::MeshModule::Shutdown()
