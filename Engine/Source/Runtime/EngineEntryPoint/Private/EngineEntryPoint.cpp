fca5b21ce074e57d4f2cd06a0c597a6dfa2c50c0
Colibri Shin <colibrishin@gmail.com>
2025-01-29 16:18:47 +0900

    test: load particle renderer task module

--- Engine/Source/Runtime/EngineEntryPoint/Private/EngineEntryPoint.cpp
+++ Engine/Source/Runtime/EngineEntryPoint/Private/EngineEntryPoint.cpp
@@ -53,6 +53,7 @@ namespace Engine::Managers
 		ModuleManager::GetInstance().LoadModule(L"ModelRenderer");
 		ModuleManager::GetInstance().LoadModule(L"ModelRendererRenderTask");
 		ModuleManager::GetInstance().LoadModule(L"ParticleRenderer");
+		ModuleManager::GetInstance().LoadModule( L"ParticleRendererRenderTask" );
 		ModuleManager::GetInstance().LoadModule(L"Texture2D");
 		ModuleManager::GetInstance().LoadModule(L"Shader");
 		ModuleManager::GetInstance().LoadModule(L"Shape");
