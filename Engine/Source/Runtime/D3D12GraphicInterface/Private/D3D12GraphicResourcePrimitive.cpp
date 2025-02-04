28ebf42c5ce16645141418b531134db111a0a8ef
Colibri Shin <colibrishin@gmail.com>
2025-02-04 21:14:42 +0900

    refactor: graphic primitive wrapper for d3d12

---
+++ Engine/Source/Runtime/D3D12GraphicInterface/Private/D3D12GraphicResourcePrimitive.cpp
@@ -0,0 +1,20 @@
+﻿#include "D3D12GraphicResourcePrimitive.h"
+#include "D3D12GraphicResourcePrimitive.generated.h"
+
+namespace Engine
+{
+    void D3D12GraphicResourcePrimitive::SetResource(void* resource)
+    {
+        GraphicResourcePrimitive::SetResource( resource );
+        m_raw_resources_ = static_cast<ID3D12Resource*>(resource);
+    }
+
+    void D3D12GraphicResourcePrimitive::Release()
+    {
+        if (m_raw_resources_)
+        {
+            m_raw_resources_->Release();
+            SetResource(nullptr);   
+        }
+    }
+}
