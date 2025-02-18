fffad055cfd3b960bb872db0c2c9e418a2c0eea8
Colibri Shin <colibrishin@gmail.com>
2025-02-18 16:11:25 +0900

    feat: install graphic tools for debugging

--- Setup.bat
+++ Setup.bat
@@ -1,4 +1,32 @@
 @echo off
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+
+echo [Install Graphic Tools for debugging]
+dism /online /add-capability /capabilityname:Tools.Graphics.DirectX~~~~0.0.1.0
+
 echo [Build Sharpmake]
 dotnet build --configuration Release Programs\Sharpmake\Sharpmake.Application\Sharpmake.Application.csproj
 
