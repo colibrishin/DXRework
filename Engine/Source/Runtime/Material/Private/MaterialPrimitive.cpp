b5f15e8cfea16d19e5513cea4dbf1db579edb8fe
Colibri Shin <colibrishin@gmail.com>
2025-02-26 17:39:19 +0900

    chore: cleanup

--- Engine/Source/Runtime/Material/Private/MaterialPrimitive.cpp
+++ Engine/Source/Runtime/Material/Private/MaterialPrimitive.cpp
@@ -4,11 +4,12 @@
 
 #include "SIMDExtension.hpp"
 
-void Engine::Graphics::MaterialPrimitive::Apply(SBs::InstanceSB& instance) const
+void Engine::Graphics::MaterialPrimitive::Apply( SBs::InstanceSB& instance ) const
 {
-    SIMDExtension::_mm256_memcpy(instance.EvaluateAddress<float>(1), this, sizeof(float) * 4);
-    SIMDExtension::_mm256_memcpy(instance.EvaluateAddress<Vector4>(0), &overrideColor, sizeof(Vector4) * 3);
+    SIMDExtension::_mm256_memcpy( instance.EvaluateAddress<float>( 1 ), this, sizeof( float ) * 4 );
+    SIMDExtension::_mm256_memcpy( instance.EvaluateAddress<Vector4>( 0 ), &overrideColor, sizeof( Vector4 ) * 3 );
     SIMDExtension::_mm256_memcpy( instance.EvaluateAddress<int>( 8 ),
                                   &repeatTexture,
-                                  sizeof( int ) * ( g_max_texture_per_material + g_max_texture_per_material + g_max_texture_per_material + 2 ) );
+                                  sizeof( int ) * ( g_max_texture_per_material + g_max_texture_per_material +
+                                                    g_max_texture_per_material + 2 ) );
 }
