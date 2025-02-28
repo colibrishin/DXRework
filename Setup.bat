e4a4bc2ccad22935029de0c13b3f99799bb868eb
Colibri Shin <colibrishin@gmail.com>
2025-02-28 09:31:52 +0900

    ci: utilize build programs from the vs build tools

--- Setup.bat
+++ Setup.bat
@@ -27,9 +27,9 @@ if '%errorlevel%' NEQ '0' (
 echo [Install Graphic Tools for debugging]
 dism /online /add-capability /capabilityname:Tools.Graphics.DirectX~~~~0.0.1.0
 
-REM echo [Install latest build tool]
-REM bitsadmin /transfer vsbuildtool /download /priority FOREGROUND "https://aka.ms/vs/17/release/vs_BuildTools.exe" "%TEMP%\vsbuildtool.exe"
-REM start /b /wait "" "%TEMP%/vsbuildtool.exe" "--passive" "--wait" "--add" "Microsoft.VisualStudio.Workload.VCTools;includeRecommended" "--add" "Microsoft.VisualStudio.Component.Windows11SDK.22621^" --add "Microsoft.VisualStudio.Component.VC.CMake.Project^" "--add" "Microsoft.VisualStudio.Workload.MSBuildTools;includeRecommended"
+echo [Install latest build tool]
+bitsadmin /transfer vsbuildtool /download /priority FOREGROUND "https://aka.ms/vs/17/release/vs_BuildTools.exe" "%TEMP%\vsbuildtool.exe"
+start /b /wait "" "%TEMP%/vsbuildtool.exe" "--passive" "--wait" "--add" "Microsoft.VisualStudio.Workload.VCTools;includeRecommended" "--add" "Microsoft.VisualStudio.Component.Windows11SDK.22621^" --add "Microsoft.VisualStudio.Component.VC.CMake.Project^" "--add" "Microsoft.VisualStudio.Workload.MSBuildTools;includeRecommended" "--add" "Microsoft.NetCore.Component.Runtime.9.0^" "--add" "Microsoft.VisualStudio.Component.Vcpkg" "--add" "Microsoft.VisualStudio.Component.VC.CLI.Support^" "--add" "Microsoft.VisualStudio.Component.VC.ATLMFC^" "--addProductLang" "en-us"
 
 echo [Build Sharpmake]
 dotnet build --configuration Release Programs\Sharpmake\Sharpmake.Application\Sharpmake.Application.csproj
@@ -46,49 +46,22 @@ rustup toolchain install nightly
 cargo +nightly b -r
 popd
 
-IF EXIST "%PROGRAMFILES%\Microsoft Visual Studio\2022\Community" (
-    SET "VSPath=%PROGRAMFILES%\Microsoft Visual Studio\2022\Community"
-) else if EXIST "%PROGRAMFILES%\Microsoft Visual Studio\2022\Professional" (
-    SET "VSPath=%PROGRAMFILES%\Microsoft Visual Studio\2022\Professional"
-) else if EXIST "%PROGRAMFILES%\Microsoft Visual Studio\2022\Enterprise" (
-    SET "VSPath=%PROGRAMFILES%\Microsoft Visual Studio\2022\Enterprise"
+IF EXIST "%PROGRAMFILES(X86)%\Microsoft Visual Studio\2022\BuildTools" (
+SET "VSPath=%PROGRAMFILES(X86)%\Microsoft Visual Studio\2022\BuildTools"
 ) else (
-    echo "No Visual Studio has been found"
-    exit /b
+echo No Visual Studio Build tools has been found
+exit /b
 )
 
-SET Cmake="cmake"
 SET "VSDevEnv=%VSPath%\VC\Auxiliary\Build\vcvars64.bat"
 call "%VSDevEnv%"
 
-where cmake
-IF errorlevel 1 (
-echo [Download CMake]
-IF NOT EXIST Programs\CMake (
-bitsadmin /transfer cmakedownload /download /priority FOREGROUND "https://github.com/Kitware/CMake/releases/download/v3.31.4/cmake-3.31.4-windows-x86_64.zip" "%TEMP%\cmake.zip"
-powershell Expand-Archive %TEMP%\cmake.zip -DestinationPath Programs\CMake)
-SET Cmake="..\CMake\cmake-3.31.4-windows-x86_64\bin\cmake.exe"
-)
-
 echo [Build header-parser]
 pushd Programs\header-parser
-start /b /wait "" %Cmake% "CMakeLists.txt"
-start /b /wait "" %Cmake% "--build" "." "--config" "Release"
+start /b /wait "" "cmake" "CMakeLists.txt"
+start /b /wait "" "cmake" "--build" "." "--config" "Release"
 popd
 
 echo [Run VCPKG]
-IF NOT EXIST Programs\vcpkg (
-pushd Programs
-git clone https://github.com/Microsoft/vcpkg.git
-popd
-)
-
-IF NOT EXIST Programs\vcpkg\vcpkg.exe (
-pushd Programs\vcpkg
-call bootstrap-vcpkg.bat
-popd
-)
-
-set "VcpkgPath=Programs\vcpkg\vcpkg.exe"
 IF NOT EXIST vcpkg_installed (mkdir vcpkg_installed)
-IF EXIST "%VcpkgPath%" ("%VcpkgPath%" install --x-install-root=%cd%\vcpkg_installed)
+vcpkg install --x-install-root=%cd%\vcpkg_installed
